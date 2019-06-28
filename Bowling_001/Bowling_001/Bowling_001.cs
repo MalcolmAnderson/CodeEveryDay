06337d2cc1f13a6f2a20699b9a7bdad505471b36
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-06-28 08:53:33 -0700

    refactor code to prepare for strike logic

--- Bowling_001/Bowling_001/Bowling_001.cs
+++ Bowling_001/Bowling_001/Bowling_001.cs
@@ -4,32 +4,40 @@ namespace Bowling_001
 {
     public class Game
     {
-        private int score = 0;
+        //private int score = 0;
         private int[] rolls = new int[21];
         private int currentRoll = 0;
 
 
         public int Score {
             get {
-                int score = 0;
-                int i = 0;
+                int rollIndex = 0;
                 int currentFrameScore = 0;
                 for(int frame=0; frame< 10; frame++)
                 {
-                    currentFrameScore = rolls[i] + rolls[i + 1];
-                    score += currentFrameScore;
-                    if (currentFrameScore == 10) // spare
-                        score += rolls[i + 2];
+                    if (isSpare(rollIndex))
+                    {
+                        currentFrameScore += 10 + rolls[rollIndex + 2];
+                    }
+                    else
+                    {
+                        currentFrameScore += rolls[rollIndex] + rolls[rollIndex + 1];
+                    }
 
-                    i += 2;
+                    rollIndex += 2;
                 }
-                return score; 
+                return currentFrameScore; 
             }
+        }
+
+        private bool isSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
         }
 
         public void Roll (int pins)
         {
-            score += pins;
+            //score += pins;
             rolls[currentRoll++] = pins;
         }
     }
