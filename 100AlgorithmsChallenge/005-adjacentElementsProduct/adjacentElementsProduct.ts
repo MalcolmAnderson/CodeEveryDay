28fc2fc25259d83dc9a741866ace1abed9442620
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-08 08:23:30 -0700

    Refactor - and add additional tests

--- 100AlgorithmsChallenge/005-adjacentElementsProduct/adjacentElementsProduct.ts
+++ 100AlgorithmsChallenge/005-adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,8 +1,15 @@
-function adjacentElementsProduct(): number{
-    return 5;
+function adjacentElementsProduct(inputArray: number[]): number{
+    let highest: number = 0;
+    let candidate: number = 0;
+    for(let i: number = 0; i<inputArray.length - 1; i++){
+        candidate = inputArray[i] * inputArray[i+1];
+        highest = candidate > highest ? candidate : highest;
+    }
+    return highest;
 
 
 }
 
-Console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
+// should be 21
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
 
