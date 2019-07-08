387d02b927a99135dd9105dc09c7de7fc817cc6c
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-07 17:26:27 -0700

    refactor addBoard for readability

--- 100AlgorithmsChallenge/addBorder/addBorder.ts
+++ 100AlgorithmsChallenge/addBorder/addBorder.ts
@@ -1,60 +1,51 @@
-function getLongestLine(myInput: string[]): number{
-    console.log(myInput[1].length)
-    console.log(myInput[0].length)
-    console.log(myInput.length)
+function getLongestLine(myInput: string[]): number {
     var longestLine = 0;
-    for (var i=0; i<myInput.length; i++){
-        if (myInput[i].length > longestLine){
+    for (var i = 0; i < myInput.length; i++) {
+        if (myInput[i].length > longestLine) {
             longestLine = myInput[i].length;
         }
     }
     return longestLine;
 }
 
-
-
-function addBorder(myInput: string[]): string[]{
-
-    //const longestLine: number = 6;
+function addBorder(myInput: string[]): string[] {
     var longestLine = getLongestLine(myInput);
+    let wallChar = '*';
+    let fillerChar = '.'; // swap ' ' and '.' for production vs debugging
 
+    let wall = wallChar.repeat(longestLine + 2); 
 
-    var output: string[] = [];
-    
-
-    let wall = '*'.repeat(longestLine+2);
-
-    for (var i =0; i<myInput.length; i++){
-        var spaces = longestLine - myInput[0].length;
-        var filler = "";
-        if (spaces > 0){
-            filler = '.'.repeat(spaces); // swap ' ' and '.' for production vs debugging
-        }
-        else{
-            filler = "";
-        }
-        var replacement = "*" + myInput[i] + filler + "*";
-        myInput[i] = replacement;
+    for (var i = 0; i < myInput.length; i++) {
+        var fillerLength = longestLine - myInput[i].length;
+        myInput[i] = generateReplacetmentLine(myInput[i], fillerLength, wallChar, fillerChar);;
     }
-
-
-    var test = myInput[0]
 
     myInput.unshift(wall);
     myInput.push(wall);
 
     return myInput;
-
-    //return ["*****","*a   *"]
-
 }
 
-console.log(getLongestLine(["a","abcd"]))
 
-console.log(addBorder(["a","abc"]));
-console.log(addBorder(["a","abcde"]));
-//console.log(addBorder(2, 4, 6, 8, 10));
+function generateReplacetmentLine(inputLine, fillerLength, wallChar, fillerChar) {
+    var filler = "";
+    if (fillerLength > 0) {
+        filler = fillerChar.repeat(fillerLength); 
+    }
+    else {
+        filler = "";
+    }
+    var replacement = wallChar + inputLine + filler + wallChar;
+    return replacement;
+}
 
+
+console.log(generateReplacetmentLine(["a"], 4, '*', '.'))
+
+console.log(getLongestLine(["a", "abcd"]))
+
+console.log(addBorder(["a", "abc"]));
+console.log(addBorder(["a", "abcde"]));
 
 // a
 // abc
