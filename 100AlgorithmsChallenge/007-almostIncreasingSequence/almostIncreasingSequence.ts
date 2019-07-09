d776ea6475b1c6e141465f70ebda7d3c5ad440b0
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-08 20:49:40 -0700

    Setup for number 7 - almostIncreasingSequence

---
+++ 100AlgorithmsChallenge/007-almostIncreasingSequence/almostIncreasingSequence.ts
@@ -0,0 +1,15 @@
+function almostIncreasingSequence(inputArray: number[]): boolean {
+    return true;
+}
+
+// should return true
+console.log(almostIncreasingSequence([1, 2, 2, 3, 5, 7]))
+
+// should return true
+console.log(almostIncreasingSequence([1, 3, 2]))
+
+// should return false
+console.log(almostIncreasingSequence([1, 2, 2, 1]))
+
+// should return true
+console.log(almostIncreasingSequence([1, 2, 23, 3, 5, 7]))
