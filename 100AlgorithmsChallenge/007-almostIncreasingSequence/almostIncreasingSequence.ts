b1f32c90775acd843795b39afbaa47be737da564
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-09 09:42:50 -0700

    A007 - Figured out how splice works

--- 100AlgorithmsChallenge/007-almostIncreasingSequence/almostIncreasingSequence.ts
+++ 100AlgorithmsChallenge/007-almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,32 @@
-function almostIncreasingSequence(inputArray: number[]): boolean {
+function isIncreasingSequence(inputArray: number[]): boolean {
+    let alwaysIncreasing: boolean = true;
+    let base: number = inputArray[0];
+    let candidate: number;
+    for (let i: number = 1; i < inputArray.length; i++) {
+        candidate = inputArray[i];
+        if (candidate > base) {
+            base = candidate;
+        } else {
+            return false;
+        }
+    }
     return true;
 }
+
+function almostIncreasingSequence(inputArray: number[]): boolean {
+    if(isIncreasingSequence(inputArray)){
+        return true;
+    }
+    let candidate: number[];
+    for (let i: number = 1; i < inputArray.length; i++) {
+        candidate = inputArray.splice(i,1);
+        isIncreasingSequence(inputArray) {
+            return true;
+        }
+    }
+    return false;
+}
+
 
 // should return true
 console.log(almostIncreasingSequence([1, 2, 2, 3, 5, 7]))
@@ -13,3 +39,21 @@ console.log(almostIncreasingSequence([1, 2, 2, 1]))
 
 // should return true
 console.log(almostIncreasingSequence([1, 2, 23, 3, 5, 7]))
+
+
+
+
+// should return false
+console.log(isIncreasingSequence([1, 2, 2, 3, 5, 7]))
+
+// should return false
+console.log(isIncreasingSequence([1, 3, 2]))
+
+// should return false
+console.log(isIncreasingSequence([1, 2, 2, 1]))
+
+// should return false
+console.log(isIncreasingSequence([1, 2, 23, 3, 5, 7]))
+
+// should return true
+console.log(isIncreasingSequence([1, 2, 3, 5, 7]))
