b7c910a600bf5c387c284807cfd701716ce485cd
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-11 06:08:44 -0700

    Add test to validate no return from forEach

---
+++ 100AlgorithmsChallenge/009-alphabetSubSequence/alphabetSubSequence.ts
@@ -0,0 +1,40 @@
+function alphabetSubSequence(inputString: string):boolean{
+    let stringArray: string[] = inputString.split("");
+    let currentLetterNumber = 0;
+    let returnCode:boolean = true;
+    stringArray.forEach((letter: string)=>{
+        if(letter.charCodeAt(0)>currentLetterNumber){
+            currentLetterNumber = letter.charCodeAt(0);
+        }
+        else{
+            returnCode = false;
+        }
+    })
+    return returnCode;
+}
+
+// should return true
+console.log(alphabetSubSequence("aceg"));
+console.log(alphabetSubSequence("ace"));
+console.log(alphabetSubSequence("bxz"));
+// should return false
+console.log(alphabetSubSequence("effg"));
+console.log(alphabetSubSequence("cdce"));
+
+function testReturnFalseInsideAForEach():boolean{
+    let myString: string = "this is a test";
+    let stringArray: string[] = myString.split("");
+    stringArray.forEach((letter: string)=>{
+        return false;
+        if (false){
+            console.log("hello");
+        }
+        else{
+            return false;
+        }
+
+    })
+    return true;
+}
+
+console.log(testReturnFalseInsideAForEach());
