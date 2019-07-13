d156d3f1d416260ffd5352dd8499971d4ca23308
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-12 21:10:48 -0700

    Solve a_011

--- 100AlgorithmsChallenge/011-areEquallyStrong/areEquallyStrong.ts
+++ 100AlgorithmsChallenge/011-areEquallyStrong/areEquallyStrong.ts
@@ -1,3 +1,10 @@
-function areEquallyStrong():boolean{
-    return false;
+function areEquallyStrong(lrlr: number[]):boolean{
+    let personOne = lrlr[0] + lrlr[1];
+    let personTwo = lrlr[2] + lrlr[3];
+    return personOne === personTwo;
 }
+
+console.log(areEquallyStrong([10,15,15,10]));
+console.log(areEquallyStrong([15,10,15,10]));
+console.log(areEquallyStrong([10,15,15,9]));
+
