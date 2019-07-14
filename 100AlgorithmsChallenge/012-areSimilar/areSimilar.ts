ef6944e2cb2c51f92cd1afd0cb6db01019a5a51f
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-13 21:39:07 -0700

    add comment about where to start

--- 100AlgorithmsChallenge/012-areSimilar/areSimilar.ts
+++ 100AlgorithmsChallenge/012-areSimilar/areSimilar.ts
@@ -1,35 +1,38 @@
 function areSimilar(arrayOne: number[], arrayTwo: number[]): boolean {
-    if(arrayOne.length != arrayTwo.length){
+    if (arrayOne.length != arrayTwo.length) {
         return false;
     }
-    if (allAreEqual(arrayOne,arrayTwo)){
+    if (allAreEqual(arrayOne, arrayTwo)) {
         return true;
     }
-    for(let i: number = 0; i<arrayOne.length - 1; i++){
+
+    // This should work but it doesn't
+    
+    for (let i: number = 0; i < arrayOne.length - 1; i++) {
         let candidateArrary: number[];
-        for(let j: number = i+1; j<arrayOne.length; j++){
+        for (let j: number = i + 1; j < arrayOne.length; j++) {
             candidateArrary = arrayItemSwap(arrayOne, i, j);
         }
-        if(allAreEqual(arrayTwo, candidateArrary)){
+        if (allAreEqual(arrayTwo, candidateArrary)) {
             return true;
         }
     }
     return false;
 }
 
-function allAreEqual(arrayOne: number[], arrayTwo: number[]): boolean{
+function allAreEqual(arrayOne: number[], arrayTwo: number[]): boolean {
     let allAreEqual: boolean = true;
-    for(let i: number = 0; i < arrayOne.length; i++){
-        if(arrayOne[i] !== arrayTwo[i]){
+    for (let i: number = 0; i < arrayOne.length; i++) {
+        if (arrayOne[i] !== arrayTwo[i]) {
             allAreEqual = false;
         }
     }
-    if(allAreEqual){
+    if (allAreEqual) {
         return true;
     }
 }
 
-function arrayItemSwap(sourceArray: number[], index1: number, index2: number) : number[]{
+function arrayItemSwap(sourceArray: number[], index1: number, index2: number): number[] {
     let returnArray: number[] = sourceArray.slice();
     returnArray[index1] = sourceArray[index2];
     returnArray[index2] = sourceArray[index1];
