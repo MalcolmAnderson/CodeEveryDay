981027965e7439ef05f800f9e9ebd113c203100b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-14 13:55:25 -0700

    Fix broken test, and code - a_012 complete

--- 100AlgorithmsChallenge/012-areSimilar/areSimilar.ts
+++ 100AlgorithmsChallenge/012-areSimilar/areSimilar.ts
@@ -5,20 +5,13 @@ function areSimilar(arrayOne: number[], arrayTwo: number[]): boolean {
     if (allAreEqual(arrayOne, arrayTwo)) {
         return true;
     }
-
-    // Note to self: When adding a second for loop, 
-    // make sure that the comparison is *inside* the second for loop
-
     for (let i: number = 0; i < arrayOne.length - 1; i++) {
         let candidateArrary: number[];
         for (let j: number = i + 1; j < arrayOne.length; j++) {
             candidateArrary = arrayItemSwap(arrayOne, i, j);
-            console.log("i="+i+", j="+j+", ")
-            console.log(candidateArrary);
-        }
-        // This should be *before* that end curly brace
-        if (allAreEqual(arrayTwo, candidateArrary)) {
-            return true;
+            if (allAreEqual(arrayTwo, candidateArrary)) {
+                return true;
+            }
         }
     }
     return false;
@@ -43,8 +36,8 @@ function arrayItemSwap(sourceArray: number[], index1: number, index2: number): n
     return returnArray;
 }
 
-// console.assert(areSimilar([1, 2, 3], [1, 2, 3]), "[1, 2, 3], [1, 2, 3] - Should be true");
-// console.assert(areSimilar([1, 2, 3], [2, 1, 3]), "[1, 2, 3], [2, 1, 3] - Should be true");
+console.assert(areSimilar([1, 2, 3], [1, 2, 3]), "[1, 2, 3], [1, 2, 3] - Should be true");
+console.assert(areSimilar([1, 2, 3], [2, 1, 3]), "[1, 2, 3], [2, 1, 3] - Should be true");
 console.assert(areSimilar([1, 2, 3, 4], [4, 2, 3, 1]), "[1, 2, 3, 4], [4, 2, 3, 1] - Should be true");
-// console.assert(!areSimilar([1, 2, 2], [2, 1, 1]), "[1, 2, 2], [2, 1, 1] - Should be false");
-// console.assert(!areSimilar([1, 2, 3, 4], [4, 2, 3, 1]), "[1, 2, 3, 4], [4, 2, 3, 4] - Should be true");
+console.assert(!areSimilar([1, 2, 2], [2, 1, 1]), "[1, 2, 2], [2, 1, 1] - Should be false");
+console.assert(!areSimilar([1, 2, 3, 4], [4, 2, 3, 4]), "[1, 2, 3, 4], [4, 2, 3, 4] - Should be false");
