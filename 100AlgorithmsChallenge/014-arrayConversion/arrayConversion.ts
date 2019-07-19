359be9a3f230f88760aa72bca7ca40320abf9195
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-19 16:00:44 -0700

    a_014 - Do it TDD, give it a failing test.

--- 100AlgorithmsChallenge/014-arrayConversion/arrayConversion.ts
+++ 100AlgorithmsChallenge/014-arrayConversion/arrayConversion.ts
@@ -1,21 +1,30 @@
 function arrayConversion(myArray: number[]): number {
 
-
     let returnValue: number = 0;
+    if (myArray.length === 1) {
+        returnValue = myArray[0];
+    }
+
+
     return returnValue;
 }
 
 
 
-function getSum(value1: number, value2: number): number{
+function getSum(value1: number, value2: number): number {
     return value1 + value2
 }
-function getProduct(value1: number, value2: number): number{
+function getProduct(value1: number, value2: number): number {
     return value1 * value2
 }
 
+
+
+console.log(arrayConversion([8]));
+console.assert(arrayConversion([8]) === 8, "arrayConversion([8]) should return 8");
+
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
-console.assert(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8])===186, "arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]) should return 186");
+console.assert(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]) === 186, "arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]) should return 186");
 
 console.log(arrayConversion([7, 8, 2]));
-console.assert(arrayConversion([7, 8, 2])===30, "arrayConversion([7, 8, 2]) should return 30");
+console.assert(arrayConversion([7, 8, 2]) === 30, "arrayConversion([7, 8, 2]) should return 30");
