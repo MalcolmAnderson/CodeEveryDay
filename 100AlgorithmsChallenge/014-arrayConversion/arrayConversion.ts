7950d6bf0bbc092df1cf13faaa9467c2a762b7fb
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-19 15:57:17 -0700

    Set up function, helper functions and tests

--- 100AlgorithmsChallenge/014-arrayConversion/arrayConversion.ts
+++ 100AlgorithmsChallenge/014-arrayConversion/arrayConversion.ts
@@ -1,4 +1,21 @@
-function arrayConversion(myArray: number[]): string[]{
-    let returnValue: string[] = [];
+function arrayConversion(myArray: number[]): number {
+
+
+    let returnValue: number = 0;
     return returnValue;
 }
+
+
+
+function getSum(value1: number, value2: number): number{
+    return value1 + value2
+}
+function getProduct(value1: number, value2: number): number{
+    return value1 * value2
+}
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+console.assert(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8])===186, "arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]) should return 186");
+
+console.log(arrayConversion([7, 8, 2]));
+console.assert(arrayConversion([7, 8, 2])===30, "arrayConversion([7, 8, 2]) should return 30");
