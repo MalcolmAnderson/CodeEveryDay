392b7a63cd434f9d471144c1eec1981c059c0ee9
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-20 22:47:40 -0700

    Added guard statement recursive version

--- 100AlgorithmsChallenge/014-arrayConversion/arrayConversionRecursive.ts
+++ 100AlgorithmsChallenge/014-arrayConversion/arrayConversionRecursive.ts
@@ -5,6 +5,9 @@ function arrayConversion2(myArray: number[]): number {
 
 
 function processEachPairOfElements2(myArray: number[], isOdd: boolean): number[] {
+    if(myArray.length === 1){
+        return myArray;
+    }
     let returnArray: number[] = [];
     for (let i: number = 0; i < myArray.length; i += 2) {
         if (i + 1 > myArray.length - 1) {
@@ -21,13 +24,13 @@ function processEachPairOfElements2(myArray: number[], isOdd: boolean): number[]
     return returnArray;
 }
 console.log(arrayConversion2([8, 2, 3]));
-// console.assert(arrayConversion2([8, 2, 3]) === 30, "arrayConversion([8, 2, 3]) should return 30");
+console.assert(arrayConversion2([8, 2, 3]) === 30, "arrayConversion([8, 2, 3]) should return 30");
 
-// console.log(arrayConversion2([8, 2]));
-// console.assert(arrayConversion2([8, 2]) === 10, "arrayConversion([8, 2]) should return 10");
+console.log(arrayConversion2([8, 2]));
+console.assert(arrayConversion2([8, 2]) === 10, "arrayConversion([8, 2]) should return 10");
 
-// console.log(arrayConversion2([8]));
-// console.assert(arrayConversion2([8]) === 8, "arrayConversion([8]) should return 8");
+console.log(arrayConversion2([8]));
+console.assert(arrayConversion2([8]) === 8, "arrayConversion([8]) should return 8");
 
 
 
