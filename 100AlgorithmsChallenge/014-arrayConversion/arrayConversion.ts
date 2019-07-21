74dc6de7843ac469dd77c8b82fe658040cdf5c06
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-20 19:20:20 -0700

    a_014 doesn't work, but structure is better now

--- 100AlgorithmsChallenge/014-arrayConversion/arrayConversion.ts
+++ 100AlgorithmsChallenge/014-arrayConversion/arrayConversion.ts
@@ -1,27 +1,44 @@
 function arrayConversion(myArray: number[]): number {
-
     let returnValue: number = 0;
     if (myArray.length === 1) {
         returnValue = myArray[0];
+    } else {
+        let runNumber: number = 0;
+        while (myArray.length > 1) {
+            if (runNumber % 2 === 0) {
+                myArray = sumEachPairOfElements(myArray);
+            }
+            else {
+                myArray = multiplyEachPairOfElements(myArray);
+            }
+            runNumber++;
+        }
     }
+    return myArray[0];
+}
+
+function sumEachPairOfElements(myArray: number[]): number[]{
+    return [8];
+}
 
 
-    return returnValue;
+function multiplyEachPairOfElements(myArray: number[]): number[]{
+    return [8];
 }
 
 
 
-function getSum(value1: number, value2: number): number {
-    return value1 + value2
-}
-function getProduct(value1: number, value2: number): number {
-    return value1 * value2
-}
 
+console.log(arrayConversion([8, 2, 3]));
+console.assert(arrayConversion([8, 2, 3]) === 30, "arrayConversion([8, 2, 3]) should return 30");
 
+console.log(arrayConversion([8, 2]));
+console.assert(arrayConversion([8, 2]) === 10, "arrayConversion([8, 2]) should return 10");
 
 console.log(arrayConversion([8]));
 console.assert(arrayConversion([8]) === 8, "arrayConversion([8]) should return 8");
+
+
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
 console.assert(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]) === 186, "arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]) should return 186");
