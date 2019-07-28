7aedb796d9cd321ad1b64ff8ced8a9a731493450
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-27 23:42:09 -0700

    a_019 - refactor to while loop

--- 100AlgorithmsChallenge/019-avoidObstacles/avoidObstacles.ts
+++ 100AlgorithmsChallenge/019-avoidObstacles/avoidObstacles.ts
@@ -1,8 +1,9 @@
 function avoidObstacles(inputArray: number[]): number {
     let sortedArray: number[] = inputArray.sort(sortNumber);
-    let smallestJump = sortedArray[sortedArray.length-1] + 1;
-    for(let i: number = smallestJump - 1; i>=0; i--){
-        if(jumpAvoidsObstacles(sortedArray, i)){
+    let smallestJump = sortedArray[sortedArray.length - 1] + 1;
+    //for (let i: number = smallestJump - 2; i >= 0; i--) {
+    for (let i: number = smallestJump - 2; i > 1; i--) {
+        if (jumpAvoidsObstacles(sortedArray, i)) {
             smallestJump = i;
         }
     }
@@ -11,31 +12,40 @@ function avoidObstacles(inputArray: number[]): number {
 
 function sortNumber(a, b) {
     return a - b;
-  }
+}
 
-function jumpAvoidsObstacles(inputArray, jumpSizeCandidate){
-    let highestNumber = inputArray[inputArray.length -1];
-    for(let i: number = 0; i < 10; i++){
-        if(i*jumpSizeCandidate > highestNumber){
-            return true;
-        }
-        if(inputArray.includes(i*jumpSizeCandidate)){
+function jumpAvoidsObstacles(inputArray: number[], jumpSizeCandidate:number) {
+    let highestNumber = inputArray[inputArray.length - 1];
+    let i: number = 1;
+    while ((i * jumpSizeCandidate) <= highestNumber) {
+        if (inputArray.includes(i * jumpSizeCandidate)) {
             return false;
         }
-        
+        i++;
     }
-    return false;
+    return true;
 }
 
 let testArray: number[] = [6, 3, 5, 9, 7];
 let testArray2: number[] = [1, 3, 5, 9, 7];
 let testArray3: number[] = [1, 2, 5, 11, 7, 14];
 let testArray4: number[] = [1, 2, 5, 11, 7, 15];
+let testArray5: number[] = [1, 3, 5, 11, 7, 25, 26, 15];
+let testArray6: number[] = [1, 3, 5, 11, 7, 25, 27, 15];
+let testArray7: number[] = [1, 3];
 console.log(testArray.includes(3));
+console.assert(avoidObstacles(testArray7) === 2, "avoidObstacles(array7) should return 2");
+console.assert(avoidObstacles(testArray6) === 2, "avoidObstacles(array6) should return 2");
+console.assert(avoidObstacles(testArray5) === 4, "avoidObstacles(array5) should return 4");
+console.assert(avoidObstacles(testArray4) === 4, "avoidObstacles(array4) should return 4");
+console.assert(avoidObstacles(testArray3) === 3, "avoidObstacles(array3) should return 3");
 console.assert(avoidObstacles(testArray2) === 2, "avoidObstacles(array2) should return 2");
 console.assert(avoidObstacles(testArray) === 4, "avoidObstacles(array) should return 4");
 console.log(avoidObstacles([6, 3, 5, 9, 7]));
 console.log(avoidObstacles(testArray3));
- console.log(avoidObstacles(testArray4));
+console.log(avoidObstacles(testArray4));
+console.log(avoidObstacles(testArray5));
+console.log(avoidObstacles(testArray6));
+console.log(avoidObstacles(testArray7));
 console.log((testArray.sort()));
 console.log(testArray);
--- 100AlgorithmsChallenge/019-avoidObstacles/out/avoidObstacles.js
+++ 100AlgorithmsChallenge/019-avoidObstacles/out/avoidObstacles.js
@@ -1,7 +1,7 @@
 function avoidObstacles(inputArray) {
     var sortedArray = inputArray.sort(sortNumber);
     var smallestJump = sortedArray[sortedArray.length - 1] + 1;
-    for (var i = smallestJump - 1; i >= 0; i--) {
+    for (var i = smallestJump - 2; i >= 0; i--) {
         if (jumpAvoidsObstacles(sortedArray, i)) {
             smallestJump = i;
         }
@@ -13,26 +13,30 @@ function sortNumber(a, b) {
 }
 function jumpAvoidsObstacles(inputArray, jumpSizeCandidate) {
     var highestNumber = inputArray[inputArray.length - 1];
-    for (var i = 0; i < 10; i++) {
-        if (i * jumpSizeCandidate > highestNumber) {
-            return true;
-        }
+    var i = 1;
+    while ((i * jumpSizeCandidate) <= highestNumber) {
         if (inputArray.includes(i * jumpSizeCandidate)) {
             return false;
         }
+        i++;
     }
-    return false;
+    return true;
 }
 var testArray = [6, 3, 5, 9, 7];
 var testArray2 = [1, 3, 5, 9, 7];
 var testArray3 = [1, 2, 5, 11, 7, 14];
 var testArray4 = [1, 2, 5, 11, 7, 15];
-// console.log(testArray.includes(3));
+var testArray5 = [1, 3, 5, 11, 7, 25, 26, 15];
+var testArray6 = [1, 3, 5, 11, 7, 25, 27, 15];
+var testArray7 = [1, 3];
+//console.log(testArray.includes(3));
 // console.assert(avoidObstacles(testArray2) === 2, "avoidObstacles(array2) should return 2");
 // console.assert(avoidObstacles(testArray) === 4, "avoidObstacles(array) should return 4");
 // console.log(avoidObstacles([6, 3, 5, 9, 7]));
-// console.log(avoidObstacles(testArray3));
-console.log(avoidObstacles(testArray4));
+console.log(avoidObstacles(testArray7));
+// console.log(avoidObstacles(testArray4));
+// console.log(avoidObstacles(testArray5));
+// console.log(avoidObstacles(testArray6));
 // console.log((testArray.sort()));
 // console.log(testArray);
 //# sourceMappingURL=avoidObstacles.js.map
