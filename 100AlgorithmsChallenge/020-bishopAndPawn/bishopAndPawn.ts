c497b49238c5a27429488ff0652bbff7ca8828be
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-28 21:26:38 -0700

    Setup tests for all 4 corners

--- 100AlgorithmsChallenge/020-bishopAndPawn/bishopAndPawn.ts
+++ 100AlgorithmsChallenge/020-bishopAndPawn/bishopAndPawn.ts
@@ -1,5 +1,14 @@
 function bishopAndPawn(bishop: string, pawn: string): boolean {
-
+    
+    return false;
 }
 
 console.log(bishopAndPawn('a1', 'c3'));
+console.assert(bishopAndPawn('a1', 'c3'), "bishopAndPawn('a1', 'c3') should equal true");
+console.assert(bishopAndPawn('f4', 'c7'), "bishopAndPawn('a1', 'c3') should equal true");
+console.assert(bishopAndPawn('d4', 'f2'), "bishopAndPawn('a1', 'c3') should equal true");
+console.assert(bishopAndPawn('h8', 'b2'), "bishopAndPawn('h8', 'b2') should equal true");
+console.assert(!bishopAndPawn('a1', 'b1'), "bishopAndPawn('a1', 'c3') should equal false");
+console.assert(!bishopAndPawn('a1', 'a4'), "bishopAndPawn('a1', 'c3') should equal false");
+console.assert(!bishopAndPawn('e5', 'g2'), "bishopAndPawn('a1', 'c3') should equal false");
+console.assert(!bishopAndPawn('h8', 'd7'), "bishopAndPawn('h8', 'd7') should equal false");
