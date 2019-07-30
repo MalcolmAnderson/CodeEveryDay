274deb4336ccabff7a64a3f8502371c7ae7618f0
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-07-30 07:50:44 -0700

    Setting up a_021 and thinking about it.

--- 100AlgorithmsChallenge/021-boxBlur/boxBlur.ts
+++ 100AlgorithmsChallenge/021-boxBlur/boxBlur.ts
@@ -2,6 +2,14 @@ function boxBlur(image: number[][]): number[][] {
 
 }
 
-console.log(boxBlur([[1, 1, 1], 
-    [1, 7, 1], 
+console.log(boxBlur(
+    [[1, 1, 1],
+    [1, 7, 1],
     [1, 1, 1]]));
+
+console.log(boxBlur([
+    [1, 1, 1, 2, 3],
+    [1, 7, 1, 2, 4],
+    [5, 6, 6, 2, 5],
+    [1, 5, 1, 2, 5],
+    [1, 1, 1, 2, 5]]));
