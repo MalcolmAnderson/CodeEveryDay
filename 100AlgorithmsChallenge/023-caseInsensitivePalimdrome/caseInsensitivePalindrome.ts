4ec670f08bb45f14d120e825281aa25c52de890a
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-04 12:57:37 -0700

    a_023 - got it in one, AND I like the solution

--- 100AlgorithmsChallenge/023-caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ 100AlgorithmsChallenge/023-caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,15 @@
 function isCaseInsensitivePalindrome(inputString: string): boolean {
-
+    for (let i: number = 0; i < inputString.length / 2; i++){
+        if(inputString[i].toLowerCase() !== inputString[inputString.length-1-i].toLowerCase()){
+            return false;
+        }
+    }
+    return true;
 }
 
+console.assert(isCaseInsensitivePalindrome('AaBaa'),"isCaseInsensitivePalindrome('AaBaa') should be true", isCaseInsensitivePalindrome('AaBaa'));
 console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.assert(isCaseInsensitivePalindrome('abac'),"isCaseInsensitivePalindrome('abac') should be false", isCaseInsensitivePalindrome('abac'));
 console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('abba'));
+console.log(isCaseInsensitivePalindrome('aba'));
