5aaf80885b08d404bc3ecb333cebd2f27ba58a73
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-06 07:56:40 -0700

    a_024 works, but there might be a better way

--- 100AlgorithmsChallenge/024-centuryFromYear/centuryFromYear.ts
+++ 100AlgorithmsChallenge/024-centuryFromYear/centuryFromYear.ts
@@ -1,5 +1,11 @@
 function centuryFromYear(year: number): number {
- 
+    let returnValue: number = 0;
+    if (year % 100 === 0){
+        returnValue = year / 100;
+    } else {
+        returnValue = (Math.floor(year / 100)+1)
+    }
+    return returnValue;
 }
 
 console.log(centuryFromYear(1905));
