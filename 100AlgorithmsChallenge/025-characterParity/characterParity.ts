621bdb616b23d38e97f510f94fcfb990cb15681e
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-09 07:15:16 -0700

    a_025 done in one

--- 100AlgorithmsChallenge/025-characterParity/characterParity.ts
+++ 100AlgorithmsChallenge/025-characterParity/characterParity.ts
@@ -1,7 +1,18 @@
 function characterParity(symbol: string): string {
-   
+   let candidate: number = parseInt(symbol);
+   if(isNaN(candidate)){
+       return "not a digit";
+   }
+   if(0 === candidate%2){
+       return "even";
+   } else {
+       return "odd";
+   }
 }
 
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
+
+console.log(parseInt("m"));
+console.log(isNaN(5));
