ce88b3f494bdaea7f447747d3ef268a21e042126
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-10 21:34:27 -0700

    completed

--- 100AlgorithmsChallenge/026-checkPalindrome/checkPalindrome.ts
+++ 100AlgorithmsChallenge/026-checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,10 @@
 function checkPalindrome(inputString: string): boolean {
+    let tempString:string = inputString.split('').reverse().join('');
+    return inputString === tempString;
 
 }
 
 console.log(checkPalindrome('aabaa'));
+console.log(checkPalindrome('Aabaa'));
 console.log(checkPalindrome('abac'));
 console.log(checkPalindrome('a'));
