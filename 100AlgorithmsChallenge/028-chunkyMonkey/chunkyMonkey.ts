73359507a7d35a79eddfe3cbc2e157169eaca06b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-12 13:43:00 -0700

    a_028 works , but probably could be smaller

--- 100AlgorithmsChallenge/028-chunkyMonkey/chunkyMonkey.ts
+++ 100AlgorithmsChallenge/028-chunkyMonkey/chunkyMonkey.ts
@@ -1,17 +1,31 @@
 function chunkyMonkey(arr: any[], size: number): any[][] {
-    return arr;
+    let buckets: any[] = [];
+    let minimumCount = Math.floor(arr.length / size);
+    let remainder = arr.length % size;
+    let currentElement: number = 0;
+    for (let i: number = 0; i < size; i++) {
+        let segment: any[] = [];
+        for (let j: number = 0; j < minimumCount; j++) {
+            segment.push(arr[currentElement++]);
+        }
+        if (remainder-- > 0) {
+            segment.push(arr[currentElement++]);
+        }
+        buckets.push(segment);
+    }
+    return buckets;
 }
 
 console.assert(
-    chunkyMonkey(["a", "b", "c", "d"], 2) === [["a", "b"], ["c", "d"]]
-    , "chunkyMonkey([\"a\", \"b\", \"c\", \"d\"], 2) should equal [[\"a\", \"b\"], [\"c\", \"d\"]] but is:"
-    , chunkyMonkey(["a", "b", "c", "d"], 2) 
-    );
+    JSON.stringify(chunkyMonkey(['a', 'b', 'c', 'd'], 2)) === JSON.stringify( [ [ 'a', 'b' ], [ 'c', 'd' ] ])
+    , "chunkyMonkey( [\'a\', \'b\', \'c\', \'d\' ], 2) should equal [ [ \'a\', \'b\' ], [ \'c\', \'d\' ] ] but is:"
+    , chunkyMonkey(["a", "b", "c", "d"], 2)
+);
 console.assert(
-    chunkyMonkey([0, 1, 2, 3, 4, 5], 4) === [["a", "b"], ["c", "d"]]
+    JSON.stringify(chunkyMonkey([0, 1, 2, 3, 4, 5], 4)) === JSON.stringify( [[0,1],[2,3],[4],[5]])
     , "chunkyMonkey([0, 1, 2, 3, 4, 5], 4) should equal [[0, 1], [2, 3], [4], [5]] but is:"
-    , chunkyMonkey([0, 1, 2, 3, 4, 5], 4) 
-    );
+    , chunkyMonkey([0, 1, 2, 3, 4, 5], 4)
+);
 
 
 console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
