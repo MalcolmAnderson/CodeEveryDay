b7b9e8b3a2c546da453a612159354987a6e85610
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-14 07:26:36 -0700

    put together my own unit testing tools

--- 100AlgorithmsChallenge/029-circleOfNumbers/circleOfNumbers.ts
+++ 100AlgorithmsChallenge/029-circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,31 @@
 function circleOfNumbers(n: number, firstNumber: number): number {
-
+    return -1;
 }
 
+
+function myIsEqualAssert(actual: string, expected: any) {
+    let assertString: string = actual + " === " + expected;
+    console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
+}
+
+function myIsTrueAssert(actual: string) {
+    console.assert(eval(actual), (actual + " should be true but is"), eval(actual));
+}
+function myIsFalseAssert(actual: string) {
+    console.assert(!eval(actual), (actual + " should be false but is"), eval(actual));
+}
+
+myIsTrueAssert("false");
+myIsFalseAssert("true");
+
+myIsEqualAssert("circleOfNumbers(10, 2)", 7);
+
+let actual: string = "circleOfNumbers(10, 2)";
+let expected = 7;
+let assertString: string = actual + " === " + expected;
+let message: string = actual + " should equal " + expected + " but is";
+console.log(assertString);
+console.log(message)
+
+console.assert(circleOfNumbers(10, 2) === 7, "circleOfNumbers(10, 2) should equal 7 but is", circleOfNumbers(10, 2));
 console.log(circleOfNumbers(10, 2));
