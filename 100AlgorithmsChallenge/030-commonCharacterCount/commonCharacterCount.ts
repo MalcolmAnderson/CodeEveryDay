ab4fb30766bf137f7dc76bc5ec06f7a09f1a6dfd
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-16 12:58:10 -0700

    Now thnigs are set up for development

--- 100AlgorithmsChallenge/030-commonCharacterCount/commonCharacterCount.ts
+++ 100AlgorithmsChallenge/030-commonCharacterCount/commonCharacterCount.ts
@@ -1,5 +1,22 @@
-function commonCharacterCount(s1: string, s2: string): number {
-   
+function assertIsEqual(actual: string, expected: any) {
+    let assertString = "";
+    if(typeof(expected)==="string"){
+        assertString = actual + " === \"" + expected +"\"";
+    }else{
+        assertString = actual + " === " + expected;
+    }
+    console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
 }
+
+function commonCharacters(s1: string, s2: string): string {
+    return "";
+ }
+
+function commonCharacterCount(s1: string, s2: string): number {
+   return -1;
+}
+
+assertIsEqual("commonCharacters('aabcc', 'adcaa')", "aac");
+assertIsEqual("commonCharacterCount('aabcc', 'adcaa')", 3);
 
 console.log(commonCharacterCount('aabcc', 'adcaa'));
