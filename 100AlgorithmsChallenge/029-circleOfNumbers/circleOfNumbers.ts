d710a961422c95b96431ca6cb4afa0843c7eb6d3
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-16 13:51:45 -0700

    fix cicrleOfNumbers

--- 100AlgorithmsChallenge/029-circleOfNumbers/circleOfNumbers.ts
+++ 100AlgorithmsChallenge/029-circleOfNumbers/circleOfNumbers.ts
@@ -1,6 +1,10 @@
-
+// I did not originally solve for firstNumber being greater than n / 2
 function circleOfNumbers(n: number, firstNumber: number): number {
+    if (firstNumber < n/2){
     return Math.floor(n/2) + firstNumber;
+    } else{
+        return firstNumber - Math.floor(n/2);
+    }
 }
 
 
@@ -20,6 +24,7 @@ assertIsTrue("false");
 assertIsFalse("true");
 
 assertIsEqual("circleOfNumbers(10, 2)", 7);
+assertIsEqual("circleOfNumbers(10, 7)", 2);
 
 let actual: string = "circleOfNumbers(10, 2)";
 let expected = 7;
@@ -30,3 +35,4 @@ console.log(message)
 
 console.assert(circleOfNumbers(10, 2) === 7, "circleOfNumbers(10, 2) should equal 7 but is", circleOfNumbers(10, 2));
 console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 7));
