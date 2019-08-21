1ddfaefdbe5ec5f97fa8bae34989e8650e20ae6e
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-20 18:33:14 -0700

    a_033 challenge is interesting

--- 100AlgorithmsChallenge/033-composeRanges/composeRanges.ts
+++ 100AlgorithmsChallenge/033-composeRanges/composeRanges.ts
@@ -1,6 +1,40 @@
-function composeRanges(nums: number[]): string[] {
+function assertIsEqual(actual: string, expected: any) {
+    let assertString = "";
+    if (typeof (expected) === "string") {
+        assertString = actual + " === \"" + expected + "\"";
+    } else {
+        assertString = actual + " === " + expected;
+    }
+    console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
+}
 
+function createSegment(startNum: number, endNum: number): string {
+    let segment: string = "";
+    if (endNum > startNum) {
+        segment = startNum.toString() + "->" + endNum.toString();
+    } else {
+        segment = startNum.toString();
+    }
+    return segment;
 }
 
 
+function composeRanges(nums: number[]): string[] {
+    let returnValue: string[] = [];
+    let startNum: number = nums[0];
+    let endNum: number = startNum;
+    for (let i: number = 1; i < nums.length; i++) {
+        if (nums[i] === endNum + 1) {
+            endNum++;
+        } else {
+            returnValue.push(createSegment(startNum, endNum));
+            startNum = nums[i];
+            endNum = startNum;
+        }
+    }
+    returnValue.push(createSegment(startNum, endNum));
+    return returnValue;
+}
+
+//assertIsEqual("composeRanges([-1, 0, 1, 2, 6, 7, 9])", [ '-1->2', '6->7', '9']);
 console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
---
+++ 100AlgorithmsChallenge/033-composeRanges/mbaTesting.ts
@@ -0,0 +1,25 @@
+// Note, Quokka doesn't support imports, so these functions are copied into main as needed.
+
+// new version
+// function assertIsEqual(actual: string, expected: any) {
+//     let assertString = "";
+//     if(typeof(expected)==="string"){
+//         assertString = actual + " === \"" + expected +"\"";
+//     }else{
+//         assertString = actual + " === " + expected;
+//     }
+//     console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
+// }
+
+// old version
+// export function assertIsEqual(actual: string, expected: any) {
+//     let assertString: string = actual + " === " + expected;
+//     console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
+// }
+
+// export function assertIsTrue(actual: string) {
+//     console.assert(eval(actual), (actual + " should be true but is"), eval(actual));
+// }
+// export function assertIsFalse(actual: string) {
+//     console.assert(!eval(actual), (actual + " should be false but is"), eval(actual));
+// }
