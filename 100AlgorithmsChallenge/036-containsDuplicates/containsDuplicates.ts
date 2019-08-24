20e304563ab8b049d99629eaf02d2c794765ee2b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-24 13:13:18 -0700

    prep for a_037

--- 100AlgorithmsChallenge/036-containsDuplicates/containsDuplicates.ts
+++ 100AlgorithmsChallenge/036-containsDuplicates/containsDuplicates.ts
@@ -1,6 +1,16 @@
 function containsDuplicates(a: number[]): boolean {
-
+    a.sort();
+    for(let i: number = 1; i < a.length; i++){
+        if(a[i] === a[i-1]){
+            return false
+        }
+    }
+    return true;
 }
 
+let test: number[] = [1, 2, 3, 1]
+console.log(test.sort());
+test = test.sort();
+console.log(test);
 console.log(containsDuplicates([1, 2, 3, 1]));
 console.log(containsDuplicates([3, 1]));
