153e16ea9e65c78dac966c03c56ddb1e2c238413
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-27 07:48:21 -0700

    kind of fun solving a_038

--- 100AlgorithmsChallenge/038-convertString/convertString.ts
+++ 100AlgorithmsChallenge/038-convertString/convertString.ts
@@ -1,5 +1,16 @@
 function convertString(s: string, t: string): boolean {
-
+    let returnValue: boolean = false;
+    let currentPositionInT: number = 0;
+    for(let i: number = 0; i < s.length; i++){
+        if(s[i]===t[currentPositionInT]){
+            currentPositionInT++;
+        }
+        if(currentPositionInT === t.length){
+            returnValue = true;
+            break;
+        }
+    }
+    return returnValue;
 }
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
