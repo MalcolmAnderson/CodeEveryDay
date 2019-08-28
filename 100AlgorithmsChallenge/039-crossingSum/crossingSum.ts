f587a85f841fdc734cd221b08176bec8c839bca5
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-28 07:59:13 -0700

    a_039 was fun

--- 100AlgorithmsChallenge/039-crossingSum/crossingSum.ts
+++ 100AlgorithmsChallenge/039-crossingSum/crossingSum.ts
@@ -1,5 +1,15 @@
+function sumOfArray(sum, num){
+    return sum + num;
+}
+
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+    let sum: number = matrix[a].reduce(sumOfArray);
+    for(let i: number = 0; i<matrix.length; i++){
+        sum += matrix[i][b];
+    }
+    sum -= matrix[a][b];
+
+    return sum;
 }
 
 console.log(crossingSum([[1, 1, 1, 1], 
