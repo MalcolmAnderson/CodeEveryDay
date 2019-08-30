936ca7a97695bd08aa5d4ca3a9a15fad60c4f21b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-30 09:20:56 -0700

    prep for a_040

--- 100AlgorithmsChallenge/040-depositProfit/depositProfit.ts
+++ 100AlgorithmsChallenge/040-depositProfit/depositProfit.ts
@@ -1,5 +1,22 @@
+// new version
+function assertIsEqual(actual: string, expected: any) {
+    let assertString = "";
+    if(typeof(expected)==="string"){
+        assertString = actual + " === \"" + expected +"\"";
+    }else{
+        assertString = actual + " === " + expected;
+    }
+    console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
+}
+
 function depositProfit(deposit: number, rate: number, threshold: number): number {
- 
+    let year: number = 0;
+    while(deposit < threshold){
+        deposit += deposit * rate / 100;
+        year++
+    }
+    return year;
 }
 
 console.log(depositProfit(100, 20, 170))
+assertIsEqual("depositProfit(100, 20, 170)", 3);
