865215429b280f0c7f3529a40e1871908315b4e3
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-31 15:35:54 -0700

    3 different ways of doing this.

--- 100AlgorithmsChallenge/041-differentSymbolsNaive/differentSymbolsNaive.ts
+++ 100AlgorithmsChallenge/041-differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,17 +1,52 @@
 function assertIsEqual(actual: string, expected: any) {
     let assertString = "";
-    if(typeof(expected)==="string"){
-        assertString = actual + " === \"" + expected +"\"";
-    }else{
+    if (typeof (expected) === "string") {
+        assertString = actual + " === \"" + expected + "\"";
+    } else {
         assertString = actual + " === " + expected;
     }
     console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
 }
 
+
+
+
+// Dylan's third way of doing it
 function differentSymbolsNaive(s: string): number {
-    let bag:string[] = [];
-    for(let i: number = 0; i < s.length; i++){
-        if(!bag.includes(s[i])){
+    const uniqueChars = new Set();
+    const inputChars: string[] = s.split('');
+    inputChars.forEach((currentChar) => {
+        uniqueChars.add(currentChar);
+    })
+    return Object.keys(uniqueChars).length;
+}
+
+// Dylan's second way of doing it
+function differentSymbolsNaive_DylansNumberTwo(s: string): number {
+    const uniqueChars: Object = {};
+    const inputChars: string[] = s.split('');
+    inputChars.forEach((currentChar) => {
+        uniqueChars[currentChar] = 1;
+    })
+    return Object.keys(uniqueChars).length;
+}
+
+// Dylan's first way of doing it
+function differentSymbolsNaive_DylansNumberOne(s: string): number {
+    const uniqueChars: string[] = [];
+    const inputChars: string[] = s.split('');
+    inputChars.forEach((currentChar) => {
+        if (!uniqueChars.includes(currentChar)) {
+            uniqueChars.push(currentChar);
+        }
+    })
+    return uniqueChars.length;
+}
+
+function differentSymbolsNaive_mine(s: string): number {
+    let bag: string[] = [];
+    for (let i: number = 0; i < s.length; i++) {
+        if (!bag.includes(s[i])) {
             bag.push(s[i]);
         }
     }
