914dc4aa00f5e285866171f3ee21d90ec2778929
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-08-31 14:05:15 -0700

    a_041 went a whole lot quicker than expected

--- 100AlgorithmsChallenge/041-differentSymbolsNaive/differentSymbolsNaive.ts
+++ 100AlgorithmsChallenge/041-differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,23 @@
-function differentSymbolsNaive(s: string): number {
- 
+function assertIsEqual(actual: string, expected: any) {
+    let assertString = "";
+    if(typeof(expected)==="string"){
+        assertString = actual + " === \"" + expected +"\"";
+    }else{
+        assertString = actual + " === " + expected;
+    }
+    console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
 }
+
+function differentSymbolsNaive(s: string): number {
+    let bag:string[] = [];
+    for(let i: number = 0; i < s.length; i++){
+        if(!bag.includes(s[i])){
+            bag.push(s[i]);
+        }
+    }
+    return bag.length;
+}
+
+assertIsEqual("differentSymbolsNaive('cabca')", 3);
 
 console.log(differentSymbolsNaive('cabca'));
