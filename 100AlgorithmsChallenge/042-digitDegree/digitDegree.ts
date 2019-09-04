e943e77ced742d887b1273655a009996383baec3
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-03 21:54:19 -0700

    save the file, get the change

--- 100AlgorithmsChallenge/042-digitDegree/digitDegree.ts
+++ 100AlgorithmsChallenge/042-digitDegree/digitDegree.ts
@@ -1,6 +1,24 @@
 function digitDegree(n: number): number {
-  
+    let holding: string[] = n.toString().split('');
+    let newNumber: number = 0;
+    let count = 0;
+    while (holding.length > 1) {
+        for(let i: number = 0; i < holding.length; i++){
+            newNumber += parseInt(holding[i]);
+        }
+        holding = newNumber.toString().split('');
+        newNumber = 0;
+        count++;
+    }
+    return count;
 }
+
+let n: number = 520;
+let holding: string[] = n.toString().split('');
+console.log(holding);
+let newNumber:number = 0;
+console.log(holding.forEach(a => { newNumber + parseInt(a) }));
+
 
 console.log(digitDegree(5));
 console.log(digitDegree(10));
