c940972ecd7c1884b10fbe1d202dffbdf1d83ae1
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-05 23:58:45 -0700

    adding a change

--- 100AlgorithmsChallenge/043-domainType/domainType.ts
+++ 100AlgorithmsChallenge/043-domainType/domainType.ts
@@ -1,5 +1,17 @@
 function domainType(domains: string[]): string[] {
-
+    let tldMap = {
+        "com" : "commercial",
+        "org" : "organization",
+        "info" : "information",
+        "net" : "network",
+    }
+    const tldTypeList: string[] = [];
+    for (let i: number = 0; i < domains.length; i++){
+        const parts = domains[i].split('.');
+        tldTypeList.push(tldMap[parts[parts.length-1]]);
+    }
+    return tldTypeList;
+    // hello world
 }
 
 console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
