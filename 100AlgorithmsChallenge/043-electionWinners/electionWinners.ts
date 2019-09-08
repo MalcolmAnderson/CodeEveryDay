4e066454b09e2868df462cb74119e17689865fc6
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-08 16:20:10 -0700

    completed a_043

--- 100AlgorithmsChallenge/043-electionWinners/electionWinners.ts
+++ 100AlgorithmsChallenge/043-electionWinners/electionWinners.ts
@@ -1,5 +1,32 @@
-function electionsWinners(votes: number[], k: number): number {
-
+function assertIsEqual(actual: string, expected: any) {
+    let assertString = "";
+    if (typeof (expected) === "string") {
+        assertString = actual + " === \"" + expected + "\"";
+    } else {
+        assertString = actual + " === " + expected;
+    }
+    console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
 }
+
+
+
+function electionsWinners(votes: number[], k: number): number {
+    let possibleWinners = votes.length;
+    let highestVoteTotal = Math.max(...votes);
+    //let sortedVotes = votes.sort(function (a, b) { return a - b });
+    for(let i: number = 0; i < votes.length; i++){
+        if ( (votes[i] + k) <= highestVoteTotal){
+            possibleWinners--;
+        }
+    }
+    return possibleWinners;
+}
+
+let points = [3, 5, 1, 2];
+points.sort(function (a, b) { return a - b });
+console.log(points);
+
+
+assertIsEqual("electionsWinners([2, 3, 5, 2], 3)", 2);
 
 console.log(electionsWinners([2, 3, 5, 2], 3));
