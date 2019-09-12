9d90d19c9e768bd688fa77187cec236ff949e5d5
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-12 08:33:33 -0700

    My solution

--- 100AlgorithmsChallenge/045-evenDigitsOnly/evenDigitsOnly.ts
+++ 100AlgorithmsChallenge/045-evenDigitsOnly/evenDigitsOnly.ts
@@ -1,6 +1,29 @@
+function assertIsEqual(actual: string, expected: any) {
+    let assertString = "";
+    if (typeof (expected) === "string") {
+        assertString = actual + " === \"" + expected + "\"";
+    } else {
+        assertString = actual + " === " + expected;
+    }
+    console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
+}
+
+
 function evenDigitsOnly(n: number): boolean {
+    const numArray: string[] = n.toString().split('');
+    let returnValue: boolean = true;
+    for (let i: number = 0; i < numArray.length; i++) {
+        if(parseInt(numArray[i]) % 2 !== 0){
+            returnValue = false;
+            break;
+        }
+    }
+    return returnValue;
 
 }
+
+assertIsEqual("evenDigitsOnly(248622)", true);
+assertIsEqual("evenDigitsOnly(642386)", false);
 
 console.log(evenDigitsOnly(248622));
 console.log(evenDigitsOnly(642386));
