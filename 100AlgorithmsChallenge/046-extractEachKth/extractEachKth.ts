4d05f3b5f13c73044f8b598818e1f7daa87ba948
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-13 10:29:47 -0700

    lots of js array functionality to learn

--- 100AlgorithmsChallenge/046-extractEachKth/extractEachKth.ts
+++ 100AlgorithmsChallenge/046-extractEachKth/extractEachKth.ts
@@ -9,13 +9,25 @@
 // }
 
 
-function extractEachKth(inputArray:number[], k: number): number[] {
-    let returnValue : number[] = [];
+// dylan's version
+function extractEachKth(inputArray: number[], k: number): number[] {
+    return inputArray.filter((element, index) => (index + 1) % k !== 0);
+}
+
+function extractEachKth_mine(inputArray: number[], k: number): number[] {
+    let returnValue: number[] = [];
+    returnValue.push(inputArray[0]);
+    for (let i: number = 1; i < inputArray.length; i++) {
+        if (!((i + 1) % k === 0)) {
+            returnValue.push(inputArray[i]);
+        }
+    }
     return returnValue;
 }
 
+
 const expected: number[] = [1, 2, 4, 5, 7, 8, 10];
 
-assertIsEqual("extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)", expected);
+//assertIsEqual("extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3)", expected);
 
 console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
