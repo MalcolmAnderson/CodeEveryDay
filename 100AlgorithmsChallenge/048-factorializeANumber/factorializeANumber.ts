75008b245253de043fb74859e831003d27e85a4e
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-15 14:57:03 -0700

    solved, should be doable recursively

--- 100AlgorithmsChallenge/048-factorializeANumber/factorializeANumber.ts
+++ 100AlgorithmsChallenge/048-factorializeANumber/factorializeANumber.ts
@@ -1,6 +1,26 @@
-function factorializeANumber(num: number): number {
-
+function assertIsEqual(actual: string, expected: any) {
+    let assertString = "";
+    if (typeof (expected) === "string") {
+        assertString = actual + " === \"" + expected + "\"";
+    } else {
+        assertString = actual.toString() + " === " + expected.toString();
+    }
+    console.assert(eval(assertString), (actual + " should equal " + expected + " but is"), eval(actual));
 }
+
+
+function factorializeANumber(num: number): number {
+    let total: number = 1;
+    for(let i: number = 2; i <= num; i++){
+        total *= i;
+    } 
+    return total;
+}
+
+assertIsEqual("factorializeANumber(5)", 120);
+assertIsEqual("factorializeANumber(10)", 3628800);
+
+
 
 console.log(factorializeANumber(5));
 console.log(factorializeANumber(10));
