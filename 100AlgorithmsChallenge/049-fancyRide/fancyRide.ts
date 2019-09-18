e44d08032dcfe11e909127d2a4e2d471ad745ffe
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-18 08:04:06 -0700

    solved a_049

--- 100AlgorithmsChallenge/049-fancyRide/fancyRide.ts
+++ 100AlgorithmsChallenge/049-fancyRide/fancyRide.ts
@@ -1,5 +1,18 @@
 function fancyRide(l: number, fares: number[]): string {
-
+    let rides = {
+        0 : "UberX",
+        1 : "UberXL",
+        2 : "UberPlus",
+        3 : "UberBlack",
+        4 : "UberSUV",
+    }   
+    const cashOnHand: number = 20;
+    // throw exception if fares.length > 4
+    for(let i: number = fares.length ; i >= 0; i--){
+        if(fares[i] * l <= cashOnHand){
+            return rides[i];
+        }
+    }
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
