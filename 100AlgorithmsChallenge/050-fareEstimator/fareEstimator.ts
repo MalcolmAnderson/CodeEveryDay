9c9a970e44a6a395e0e1bc9ebb411f0b236f8f63
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-20 13:21:07 -0700

    a_050 solved

--- 100AlgorithmsChallenge/050-fareEstimator/fareEstimator.ts
+++ 100AlgorithmsChallenge/050-fareEstimator/fareEstimator.ts
@@ -1,5 +1,10 @@
 function fareEstimator(ride_time: number, ride_distance: number, cost_per_minute: number[], cost_per_mile: number[]): number[] {
-
+    const fareCost: number[] = [];
+    let exception: boolean = cost_per_minute.length !== cost_per_mile.length;
+    for(let i: number = 0; i < cost_per_mile.length; i++){
+        fareCost.push((cost_per_minute[i] * ride_time) + (cost_per_mile[i] * ride_distance));
+    }    
+    return fareCost;
 }
 
 console.log(fareEstimator(30, 7, [0.2, 0.35, 0.4, 0.45], [1.1, 1.8, 2.3, 3.5]));
