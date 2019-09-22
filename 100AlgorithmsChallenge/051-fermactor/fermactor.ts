845f2c25420f562ff9265b0d685369d9eabd4be0
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-22 15:01:35 -0700

    a_051 is interesting.

--- 100AlgorithmsChallenge/051-fermactor/fermactor.ts
+++ 100AlgorithmsChallenge/051-fermactor/fermactor.ts
@@ -1,5 +1,25 @@
 function fermactor(n: number): number[] {
-
+    const retVal: number[] = [];
+    for (let i: number = 1; i < Math.pow(n,2); i++) {
+        while (Math.pow(i, 2) < n) {
+            i++;
+        }
+        let candidate: number = 0;
+        for (let j: number = 0; j < Math.pow(n,2); j++) {
+            candidate = Math.pow(i, 2) - Math.pow(j, 2);
+            console.log(i.toString() + " - " + j.toString() + " = " + candidate.toString() )
+            if (candidate === n) {
+                retVal.push(i);
+                retVal.push(j);
+                return retVal;
+            } else if(candidate < 0) {
+                console.log(candidate);
+                break;
+            }
+        }
+    }
+    return retVal;
 }
 
-console.log(fermactor(15));
+
+console.log(fermactor(123));
