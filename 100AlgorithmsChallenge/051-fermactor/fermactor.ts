eb5b60393e004b31cc629f6b4ea3f12aaa2b822b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-23 15:26:05 -0700

    prep for a_052

--- 100AlgorithmsChallenge/051-fermactor/fermactor.ts
+++ 100AlgorithmsChallenge/051-fermactor/fermactor.ts
@@ -1,18 +1,30 @@
+// Dylan's solution
 function fermactor(n: number): number[] {
+    for (let i: number = 1; i < Math.pow(n, 2); i++) {
+        for (let j: number = 0; j < i; j++) {
+            const total = i ** 2 - j ** 2;
+            if (total === n) {
+                return [i, j];
+            }
+        }
+    }
+}
+
+function fermactor_mine(n: number): number[] {
     const retVal: number[] = [];
-    for (let i: number = 1; i < Math.pow(n,2); i++) {
+    for (let i: number = 1; i < Math.pow(n, 2); i++) {
         while (Math.pow(i, 2) < n) {
             i++;
         }
         let candidate: number = 0;
-        for (let j: number = 0; j < Math.pow(n,2); j++) {
+        for (let j: number = 0; j < Math.pow(n, 2); j++) {
             candidate = Math.pow(i, 2) - Math.pow(j, 2);
-            console.log(i.toString() + " - " + j.toString() + " = " + candidate.toString() )
+            console.log(i.toString() + " - " + j.toString() + " = " + candidate.toString())
             if (candidate === n) {
                 retVal.push(i);
                 retVal.push(j);
                 return retVal;
-            } else if(candidate < 0) {
+            } else if (candidate < 0) {
                 console.log(candidate);
                 break;
             }
@@ -22,4 +34,4 @@ function fermactor(n: number): number[] {
 }
 
 
-console.log(fermactor(123));
+console.log(fermactor(13));
