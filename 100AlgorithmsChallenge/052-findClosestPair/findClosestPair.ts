3b768509f938004275c7bc7287afac1672b1df5c
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-24 09:22:53 -0700

    a_052 was fun

--- 100AlgorithmsChallenge/052-findClosestPair/findClosestPair.ts
+++ 100AlgorithmsChallenge/052-findClosestPair/findClosestPair.ts
@@ -1,6 +1,18 @@
 function findClosestPair(numbers: number[], sum: number): number {
-   
+   let lowestMatch: number = -1
+    for(let i: number = 0; i < numbers.length; i++){
+        for(let j: number = i + 1; j < numbers.length; j++){
+            if(numbers[i] + numbers[j] === sum){
+                if(lowestMatch < 0){
+                    lowestMatch = j - i;
+                } else if (j-i < lowestMatch) {
+                    lowestMatch = j - i;
+                }
+            }
+        }
+   }
+   return lowestMatch;
 }
 
-console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
-console.log(findClosestPair([2, 3, 7], 8));
+console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5)); // 2
+console.log(findClosestPair([2, 3, 7], 8)); // -1
