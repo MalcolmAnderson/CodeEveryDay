61d7e911d685415a2dcda94f31644886f4a1145b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-27 14:35:46 -0700

    That was fun

--- 100AlgorithmsChallenge/054-firstDigit/firstDigit.ts
+++ 100AlgorithmsChallenge/054-firstDigit/firstDigit.ts
@@ -1,5 +1,10 @@
 function firstDigit(inputString: string): string {
-
+    for(let i: number = inputString.length-1; i >= 0; i--){
+        const c = inputString[i];
+        if(c >= '0' && c <= '9'){
+            return c;
+        }
+    }
 }
 
 console.log(firstDigit('var_1__Int'));
