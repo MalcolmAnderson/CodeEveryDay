a80cc4e0924d2ce030b057d661538bb3026b8016
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-28 11:34:39 -0700

    Added dyan's solution, and fixed my original

--- 100AlgorithmsChallenge/054-firstDigit/firstDigit.ts
+++ 100AlgorithmsChallenge/054-firstDigit/firstDigit.ts
@@ -1,4 +1,17 @@
-function firstDigit(inputString: string): string {
+// Dylan's solution
+function firstDigit_Dylans(inputString: string): string {
+    const digits: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const chars: string[] = inputString.split('');
+
+    for(let i: number = 0; i < inputString.length; i++){
+        if(digits.includes(inputString[i])){
+            return inputString[i];
+        }
+    }
+}
+
+// oops, I read that as find the right most
+function firstDigit_mine(inputString: string): string {
     for(let i: number = inputString.length-1; i >= 0; i--){
         const c = inputString[i];
         if(c >= '0' && c <= '9'){
@@ -7,6 +20,16 @@ function firstDigit(inputString: string): string {
     }
 }
 
-console.log(firstDigit('var_1__Int'));
+// corrected
+function firstDigit(inputString: string): string {
+    for(let i: number = 0; i < inputString.length; i++){
+        const c = inputString[i];
+        if(c >= '0' && c <= '9'){
+            return c;
+        }
+    }
+}
+
+console.log(firstDigit('v9ar_1__Int'));
 console.log(firstDigit('q2q-q'));
 console.log(firstDigit('0ss'));
