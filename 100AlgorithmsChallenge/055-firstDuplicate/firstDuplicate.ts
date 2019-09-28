0fdd2465a2fcc6d22749891206aece0e4ac8c077
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-09-28 11:57:29 -0700

    Dylan's solution is very interesting

--- 100AlgorithmsChallenge/055-firstDuplicate/firstDuplicate.ts
+++ 100AlgorithmsChallenge/055-firstDuplicate/firstDuplicate.ts
@@ -1,4 +1,16 @@
+// dylan's
 function firstDuplicate(a: number[]): number {
+    const firstDup = {};
+    for(let num of a){
+        if(firstDup.hasOwnProperty(num)){
+            return num;
+        }
+        firstDup[num] = num;
+    }
+    return -1;
+}
+
+function firstDuplicate_mine(a: number[]): number {
     let lowestDuplicate: number = -1;
     for(let i: number = 0; i < a.length - 1; i++){
         for(let j: number = i+1; j < a.length; j++){
