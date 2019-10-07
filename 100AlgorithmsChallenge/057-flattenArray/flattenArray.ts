597273cedee596daf62cb0c0750354afaeaa9d26
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-07 10:05:51 -0700

    my first real entrance to recursion

--- 100AlgorithmsChallenge/057-flattenArray/flattenArray.ts
+++ 100AlgorithmsChallenge/057-flattenArray/flattenArray.ts
@@ -1,6 +1,21 @@
-function flattenArray(arr: any[]): any[] {
-
+function returnArrayContents(candidate: any, holdingBin: any[]): any[]{
+    console.log(Array.isArray(candidate));
+    if(!Array.isArray(candidate)){
+        holdingBin.push(candidate);
+        return holdingBin;
+    } else {
+        for(let i: number = 0; i < candidate.length; i++){
+            returnArrayContents(candidate[i], holdingBin);
+        }
+    }
 }
 
+function flattenArray(arr: any[]): any[] {
+    let returnValue: any[] = [];
+    returnArrayContents(arr, returnValue);
+    return returnValue;
+}
+
+console.log(flattenArray(["a"]));
 console.log(flattenArray([[["a"]], [["b"]]]));
 console.log(flattenArray([1, [2], [3, [[4]]]]));
