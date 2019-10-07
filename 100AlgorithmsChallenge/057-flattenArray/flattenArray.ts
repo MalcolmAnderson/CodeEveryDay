fb7ba412dab984817b7f6569d4759657f8de1d86
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-07 11:01:48 -0700

    added Dylan's version

--- 100AlgorithmsChallenge/057-flattenArray/flattenArray.ts
+++ 100AlgorithmsChallenge/057-flattenArray/flattenArray.ts
@@ -1,15 +1,36 @@
-function returnArrayContents(candidate: any, holdingBin: any[]): any[]{
-    if(!Array.isArray(candidate)){
-        holdingBin.push(candidate);
-        return holdingBin;
-    } else {
-        for(let i: number = 0; i < candidate.length; i++){
-            returnArrayContents(candidate[i], holdingBin);
-        }
+// dylan's version is functionally the same, but a little cleaner.
+// at the same time, I'm not a fan of oneArray being treated as 
+// a global rather than passing oneArray into the function and returning it.
+function flattenArray(arr: any[]): any[] {
+    const oneArray = []
+    flatten(arr);
+
+    function flatten(arr: any[]){
+        arr.forEach((element) => {
+            if (Array.isArray(element)){
+                flatten(element);
+            } else {
+                oneArray.push(element);
+            }
+        })
     }
+    return oneArray;
 }
 
-function flattenArray(arr: any[]): any[] {
+
+function flattenArray_mine(arr: any[]): any[] {
+    function returnArrayContents(candidate: any, holdingBin: any[]): any[]{
+    
+        if(!Array.isArray(candidate)){
+            holdingBin.push(candidate);
+            return holdingBin;
+        } else {
+            for(let i: number = 0; i < candidate.length; i++){
+                returnArrayContents(candidate[i], holdingBin);
+            }
+        }
+    }
+
     let returnValue: any[] = [];
     returnArrayContents(arr, returnValue);
     return returnValue;
