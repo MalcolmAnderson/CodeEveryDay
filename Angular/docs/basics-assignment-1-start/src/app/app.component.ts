cb1e73959d3b1dfdbdf404d89bd82772b66ef2ef
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-11 08:06:13 -0700

    prep for a_059

---
+++ Angular/docs/basics-assignment-1-start/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SuccessAlertComponent } from 'src/app/success-alert/success-alert.component';
+import { WarningAlertComponent } from 'src/app/warning-alert/warning-alert.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
---
+++ Angular/docs/basics-assignment-1-start/src/app/exception-alert/exception-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exception-alert',
+  templateUrl: './exception-alert.component.html',
+  styleUrls: ['./exception-alert.component.css']
+})
+export class ExceptionAlertComponent {
+
+}
