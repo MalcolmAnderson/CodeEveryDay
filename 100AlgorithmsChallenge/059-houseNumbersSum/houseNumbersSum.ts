bf9c36d7d82c5be277dc4a26f2ab2b6b42e88cc7
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-15 15:24:38 -0700

    remove angular - add a_059

--- 100AlgorithmsChallenge/059-houseNumbersSum/houseNumbersSum.ts
+++ 100AlgorithmsChallenge/059-houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,12 @@
 function houseNumbersSum(inputArray: number[]): number {
-
+    let total = 0;
+    for(let i: number = 0; i < inputArray.length; i++){
+        if(inputArray[i]===0){
+            break;
+        }
+        total += inputArray[i];
+    }
+    return total;
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
