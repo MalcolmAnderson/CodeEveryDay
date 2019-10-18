bf6f868258b339d63600a43ee6fa40b2f5f42e63
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-17 21:43:45 -0700

    it's ugly, but a_060 works

--- 100AlgorithmsChallenge/060-houseOfCats/houseOfCats.ts
+++ 100AlgorithmsChallenge/060-houseOfCats/houseOfCats.ts
@@ -1,5 +1,17 @@
 function houseOfCats(legs: number): number[] {
+    const returnValue: number[] = [];
+    let maxCats: number = Math.floor(legs / 4);
+    if(legs === 2){
+        return [1];
+    }
+    if(legs % 4 === 0){
+        returnValue.push(0);
+    } else if (legs % 4 == 2){
+        returnValue.push(1)
+    }
+    returnValue.push(legs / 2);
 
+    return returnValue;
 }
 
 console.log(houseOfCats(6));
