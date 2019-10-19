eafd54e9dd548f2efb73b8ea899256f188763274
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-19 09:32:12 -0700

    fixed: No need to return start tag

--- 100AlgorithmsChallenge/061-htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ 100AlgorithmsChallenge/061-htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,7 +1,22 @@
+// Dylan's version
+// function htmlEndTagByStartTag_dylan(startTag: string): string {
+//     const splitString = startTag.split(' ');
+//     const splitTag = splitString[0].toString().split('');
+//     let endTag = '</';
+//     for (let i: number = 0; i < splitTag.length; i++){
+//         endTag += splitTag[i];
+//     };
+//     return endTag;
+// }
+
+
+
+
+
 function htmlEndTagByStartTag(startTag: string): string {
     let startingPosition: number = startTag.indexOf('<');
     let payload: string = startTag.substring(startingPosition + 1, getEndingPosition(startTag, startingPosition));
-    return startTag + '</' + payload + '>';
+    return '</' + payload + '>';
 }
 
 function getEndingPosition(startTag: string, indexOfLessThan: number): number{
