d9279e5fb7b6356e508348410abba52f45399de6
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-19 09:42:06 -0700

    a_061 added Dylan's version

--- 100AlgorithmsChallenge/061-htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ 100AlgorithmsChallenge/061-htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,25 +1,26 @@
 // Dylan's version
-// function htmlEndTagByStartTag_dylan(startTag: string): string {
-//     const splitString = startTag.split(' ');
-//     const splitTag = splitString[0].toString().split('');
-//     let endTag = '</';
-//     for (let i: number = 0; i < splitTag.length; i++){
-//         endTag += splitTag[i];
-//     };
-//     return endTag;
-// }
-
-
-
-
-
 function htmlEndTagByStartTag(startTag: string): string {
+    const splitString = startTag.split(' ');
+    const splitTag = splitString[0].toString().split('');
+    let endTag = '</';
+    for (let i: number = 1; i < splitTag.length; i++) {
+        endTag += splitTag[i];
+    }
+    endTag = endTag[endTag.length - 1] == '>' ? endTag : endTag += '>';
+    return endTag;
+}
+
+
+
+
+
+function htmlEndTagByStartTag_mine(startTag: string): string {
     let startingPosition: number = startTag.indexOf('<');
     let payload: string = startTag.substring(startingPosition + 1, getEndingPosition(startTag, startingPosition));
     return '</' + payload + '>';
 }
 
-function getEndingPosition(startTag: string, indexOfLessThan: number): number{
+function getEndingPosition(startTag: string, indexOfLessThan: number): number {
     let indexOfGreaterThan: number = startTag.indexOf('>');
     let firstSpaceAfterStartingPosition: number = startTag.indexOf(' ', indexOfLessThan);
     const spaceNotFoundinTag: boolean = firstSpaceAfterStartingPosition === -1;
@@ -27,5 +28,4 @@ function getEndingPosition(startTag: string, indexOfLessThan: number): number{
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag(" <button type='button' disabled>"));
 console.log(htmlEndTagByStartTag('<i>'))
