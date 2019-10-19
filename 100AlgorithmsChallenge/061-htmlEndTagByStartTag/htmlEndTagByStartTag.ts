051f1d34d82790241e63c43817047b72a277d750
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-19 00:12:30 -0700

    a_061 completed

--- 100AlgorithmsChallenge/061-htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ 100AlgorithmsChallenge/061-htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,16 @@
 function htmlEndTagByStartTag(startTag: string): string {
+    let startingPosition: number = startTag.indexOf('<');
+    let payload: string = startTag.substring(startingPosition + 1, getEndingPosition(startTag, startingPosition));
+    return startTag + '</' + payload + '>';
+}
 
+function getEndingPosition(startTag: string, indexOfLessThan: number): number{
+    let indexOfGreaterThan: number = startTag.indexOf('>');
+    let firstSpaceAfterStartingPosition: number = startTag.indexOf(' ', indexOfLessThan);
+    const spaceNotFoundinTag: boolean = firstSpaceAfterStartingPosition === -1;
+    return spaceNotFoundinTag ? indexOfGreaterThan : firstSpaceAfterStartingPosition;
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+console.log(htmlEndTagByStartTag(" <button type='button' disabled>"));
 console.log(htmlEndTagByStartTag('<i>'))
