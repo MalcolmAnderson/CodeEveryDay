fc01c7902d73853171c6d39478f2e3a69d85d813
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-22 09:18:10 -0700

    a_062 completed in 4:48 minutes

--- 100AlgorithmsChallenge/062-incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ 100AlgorithmsChallenge/062-incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,24 @@
 function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
-
+    let failedAttempts: number = 0;
+    for (let i: number = 0; i < attempts.length; i++) {
+        if (attempts[i] === passcode) {
+            failedAttempts = 0;
+        } else {
+            failedAttempts++;
+        }
+        if (failedAttempts >= 10) {
+            return true;
+        }
+    }
+    return false;
 }
 
+
+// true
 console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+
+// false
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "1111", "0000", "6666", "7285", "5555", "1111"]));
+
+// true
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555"]));
