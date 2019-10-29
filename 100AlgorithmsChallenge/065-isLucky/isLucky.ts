d6294f82518ecfbfb8f30e25a096f36e35a3c7fa
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-29 08:36:44 -0700

    have to learn how to use .map( )

--- 100AlgorithmsChallenge/065-isLucky/isLucky.ts
+++ 100AlgorithmsChallenge/065-isLucky/isLucky.ts
@@ -1,4 +1,19 @@
+// Dylan's
 function isLucky(n: number): boolean {
+    const luckyN: string = n.toString();
+    const half: number = luckyN.length / 2
+    const firstHalf: number = getTotal(luckyN.substring(0, half));
+    const secondHalf: number = getTotal(luckyN.substring(half, luckyN.length));
+    return firstHalf === secondHalf;
+}
+
+function getTotal(n: string): number {
+    return n.split('').map((char) => parseInt(char)).reduce((num1, num2) => num1 + num2);
+}
+
+
+
+function isLucky_mine(n: number): boolean {
     const nAsString: string = n.toString();
     const halfLength: number = nAsString.length / 2
     console.log(nAsString)
