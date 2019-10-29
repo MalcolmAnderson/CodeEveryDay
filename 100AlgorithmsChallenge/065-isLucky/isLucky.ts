6c6397954845f6ba9760272283380801292aa521
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-29 08:11:12 -0700

    This was WAY harder than it should have been

--- 100AlgorithmsChallenge/065-isLucky/isLucky.ts
+++ 100AlgorithmsChallenge/065-isLucky/isLucky.ts
@@ -1,5 +1,24 @@
 function isLucky(n: number): boolean {
-    
+    const nAsString: string = n.toString();
+    const halfLength: number = nAsString.length / 2
+    console.log(nAsString)
+    console.log(halfLength);
+    const firstHalf = nAsString.substring(0, halfLength);
+    const secondHalf = nAsString.substring(halfLength);
+    console.log(firstHalf);
+    console.log(secondHalf);
+    return sumOfEachNumber(firstHalf) === sumOfEachNumber(secondHalf);
+}
+
+function sumOfEachNumber(numberAsString: string): number{
+    const myStrings: string[] = numberAsString.split('');
+    console.log(myStrings)
+    let total: number = 0;
+    for(let i:number = 0; i < myStrings.length; i++){
+        total += parseInt(myStrings[i]);
+    }
+    console.log(total);
+    return total;
 }
 
 console.log(isLucky(1230));
