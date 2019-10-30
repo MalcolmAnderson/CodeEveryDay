1dd749c041b5f28a7e819fcc858173c2338b7207
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-10-30 07:55:19 -0700

    set up unit tests

--- 100AlgorithmsChallenge/066-isTandemRepeat/isTandemRepeat.ts
+++ 100AlgorithmsChallenge/066-isTandemRepeat/isTandemRepeat.ts
@@ -2,6 +2,18 @@ function isTandemRepeat(inputString: string): boolean {
   
 }
 
+
+// true
 console.log(isTandemRepeat('tandemtandem'))
+
+// false
 console.log(isTandemRepeat('qqq'))
+
+// true
+console.log(isTandemRepeat('qqqq'))
+
+// true
+console.log(isTandemRepeat('2ww2ww'))
+
+// false
 console.log(isTandemRepeat('2w2ww'))
