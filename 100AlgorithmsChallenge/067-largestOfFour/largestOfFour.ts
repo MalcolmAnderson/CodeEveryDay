aa7fcd21ed487359ae2f49b30e571b440ea78892
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-01 08:24:59 -0700

    This was straightforward

--- 100AlgorithmsChallenge/067-largestOfFour/largestOfFour.ts
+++ 100AlgorithmsChallenge/067-largestOfFour/largestOfFour.ts
@@ -1,5 +1,9 @@
 function largestOfFour(nums: number[][]): number[] {
-
+    const returnArray: number[] = [];
+    for(let thisSet of nums){
+        returnArray.push(Math.max(...thisSet))
+    }
+    return returnArray;
 }
 
 console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
