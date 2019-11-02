8fd1f02da5a11ca59a18d83be8a301add5753ead
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-02 10:37:12 -0700

    recheck your questions before submitting answer

--- 100AlgorithmsChallenge/069-lateRide/lateRide.ts
+++ 100AlgorithmsChallenge/069-lateRide/lateRide.ts
@@ -1,23 +1,22 @@
 function lateRide(n: number): number {
     // minutes to hour:minutes time
     const hours: number = Math.floor(n / 60);
+
+    const dylans_minutes: number = n % 60; // better
     const minutes: number = n - hours * 60;
     const time: string = hours.toString() + minutes.toString();
     return reduceThis(time);
 }
 
 function reduceThis(numArray: string): number {
-    if (numArray.length === 1) {
-        return parseInt(numArray);
-    }
     let total: number = 0;
     for (let i: number = 0; i < numArray.length; i++) {
         total += parseInt(numArray[i]);
     }
-    return reduceThis(total.toString());
+    return total;
 }
 
-// 5
+// 11
 console.log(reduceThis("5006"))
 
 console.log(lateRide(240));
