3217d65a93dfa81ad9f74d767253775e1086dff9
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-03 08:38:11 -0800

    A_070 (launch sequence) was WORK, but very cool

--- 100AlgorithmsChallenge/070-launchSequenceChecker/launchSequenceChecker.ts
+++ 100AlgorithmsChallenge/070-launchSequenceChecker/launchSequenceChecker.ts
@@ -1,5 +1,29 @@
 function launchSequenceChecker(systemNames: string[], stepNumbers: number[]): boolean {
-
+    const systems: Object = new Object();
+    for(let i: number = 0; i < systemNames.length; i++){
+        if(!systems.hasOwnProperty(systemNames[i])){
+            systems[systemNames[i]] = [];
+        }
+        systems[systemNames[i]].push(stepNumbers[i]);
+    }
+    for (var prop in systems){
+        if (Object.prototype.hasOwnProperty.call(systems, prop)){
+            let arrayLength: number = systems[prop].length;
+            if (arrayLength > 1){
+                let currentValue = systems[prop][0];
+                for(let i: number = 1; i < arrayLength; i++){
+                    if(systems[prop][i] < currentValue){
+                        return false;
+                    } else {
+                        currentValue = systems[prop[i]];
+                    }
+                }
+            }
+            console.log(systems[prop])
+        }
+    }
+    console.log(systems);
+    return true;
 }
 
 console.log(launchSequenceChecker(["stage_1", "stage_2", "dragon", "stage_1", "stage_2", "dragon"], [1, 10, 11, 2, 12, 111]));
