3af9bed213aedbea9271ef62097186e399f64f8f
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-04 19:53:40 -0800

    A_071 - join() is the opposite of splint('')

--- 100AlgorithmsChallenge/071-longestDigitsPrefix/longestDigitsPrefix.ts
+++ 100AlgorithmsChallenge/071-longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,5 +1,17 @@
 function longestDigitsPrefix(inputString: string): string {
-
+    const prefix: string[] = [];
+    for(let i: number = 0; i < inputString.length; i++){
+        let candidate: number = parseInt(inputString[i]);
+        console.log(candidate);
+        if(isNaN(candidate)) {
+            return prefix.join();
+        } else {
+            prefix.push(inputString[i]);
+        }
+    }
+    return prefix.join();
 }
 
 console.log(longestDigitsPrefix('123aa1'));
+console.log(longestDigitsPrefix('12aa1'));
+console.log(longestDigitsPrefix('123456aa1'));
