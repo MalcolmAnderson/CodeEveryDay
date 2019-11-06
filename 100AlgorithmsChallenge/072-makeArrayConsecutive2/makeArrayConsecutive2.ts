ba63b597a4e0aa4a71f7f3c26c8f02a08d597a4e
Malcolm Anderson <malcolm.anderson@kp.org>
2019-11-06 08:25:25 -0800

    a_072 was fun and easy

--- 100AlgorithmsChallenge/072-makeArrayConsecutive2/makeArrayConsecutive2.ts
+++ 100AlgorithmsChallenge/072-makeArrayConsecutive2/makeArrayConsecutive2.ts
@@ -1,5 +1,17 @@
 function makeArrayConsecutive2(statues: number[]): number {
-
+    statues = statues.sort();
+    let numberOfNewStatues: number = 0;
+    let curretNumber: number = statues[0];
+    for(let i: number = 1; i < statues.length; i++){
+        numberOfNewStatues += statues[i] - curretNumber - 1;
+        curretNumber = statues[i];
+    }
+    return numberOfNewStatues;
 }
 
 console.log(makeArrayConsecutive2([6, 2, 3, 8]));
+console.log(makeArrayConsecutive2([8]));
+console.log(makeArrayConsecutive2([2, 3]));
+console.log(makeArrayConsecutive2([2, 4]));
+console.log(makeArrayConsecutive2([6, 3]));
+
