c91b9454766b8ed495e72df28a5e4de12976b9f4
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-08 11:24:00 -0800

    a_073 interesting issue.

--- 100AlgorithmsChallenge/073-matrixElementsSum/matrixElementsSum.ts
+++ 100AlgorithmsChallenge/073-matrixElementsSum/matrixElementsSum.ts
@@ -1,9 +1,48 @@
 function matrixElementsSum(matrix: any[][]): number {
-
+    const validRoom: number[] = [];
+    let totalCost: number = 0;
+    for(let i: number = 0; i < matrix[0].length; i++){
+        validRoom.push(1);
+    }
+    // console.log(validRoom);
+    for(let floor: number = 0; floor < matrix.length; floor++){
+        for(let room: number = 0; room < matrix[floor].length; room++){
+            // console.log(validRoom);
+            if(validRoom[room] === 1){
+                if(matrix[floor][room] === 0){
+                    validRoom[room] = 0; // disable room
+                } else {
+                    totalCost += matrix[floor][room];
+                }
+            }
+        }
+    }
+    return totalCost;
 }
+
+console.log(matrixElementsSum(
+    [[2, 0, 3, 3]]
+));
 
 console.log(matrixElementsSum(
     [[0, 1, 1, 2],
     [0, 5, 0, 0],
     [2, 0, 3, 3]]
 ));
+
+
+// matrix = 
+
+//          [[0, 1, 1, 2],
+//           [0, 5, 0, 0], 
+//           [2, 0, 3, 3]]
+// the output should be
+// matrixElementsSum(matrix) = 9.
+
+// Here's the rooms matrix with unsuitable rooms marked with 'x':
+
+//        [[x, 1, 1, 2], 
+//         [x, 5, x, x], 
+//         [x, x, x, x]]
+// Thus, the answer is 1 + 5 + 1 + 2 = 9.
+
