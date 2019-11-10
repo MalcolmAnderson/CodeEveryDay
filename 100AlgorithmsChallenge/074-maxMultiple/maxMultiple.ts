384a58aaf265381261546fb54ff30167b8a04ec4
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-10 15:25:47 -0800

    a_074 simple enough

--- 100AlgorithmsChallenge/074-maxMultiple/maxMultiple.ts
+++ 100AlgorithmsChallenge/074-maxMultiple/maxMultiple.ts
@@ -1,5 +1,6 @@
 function maxMultiple(divisor: number, bound: number): number  {
-
+    let largestMultiple: number = Math.floor(bound / divisor);
+    return divisor * largestMultiple;
 }
 
 console.log(maxMultiple(3,10));
