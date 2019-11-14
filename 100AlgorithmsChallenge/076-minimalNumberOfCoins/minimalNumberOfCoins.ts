b1e5a0077588d42ceaa366f6a4875fe60cb25242
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-14 13:11:50 -0800

    That went much smoother than expected

--- 100AlgorithmsChallenge/076-minimalNumberOfCoins/minimalNumberOfCoins.ts
+++ 100AlgorithmsChallenge/076-minimalNumberOfCoins/minimalNumberOfCoins.ts
@@ -1,5 +1,17 @@
 function minimalNumberOfCoins(coins: number[], price: number): number {
-
+    let numberOfCoins: number = 0;
+    let priceLeft: number = price;
+    for (let i: number = coins.length - 1; i >= 0; i--) {
+        while (priceLeft >= coins[i]) {
+            numberOfCoins++;
+            priceLeft -= coins[i];
+        }
+    }
+    return numberOfCoins;
 }
 
+// 6
 console.log(minimalNumberOfCoins([1, 2, 10], 28));
+
+// 5
+console.log(minimalNumberOfCoins([1, 5, 20], 28));
