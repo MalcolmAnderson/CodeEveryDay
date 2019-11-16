b4c940962e5bb4fe5f79b37b6fcb8193017e49f3
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-16 09:08:55 -0800

    a few different version of a_077

--- 100AlgorithmsChallenge/077-missingLetters/missingLetters.ts
+++ 100AlgorithmsChallenge/077-missingLetters/missingLetters.ts
@@ -1,4 +1,31 @@
 function missingLetters(str: string): string {
+    let missingLetter: string;  // undefined by design
+    let previousLetterNumber: number = str[0].charCodeAt(0);
+    for (let i: number = 1; i < str.length; i++) {
+        if(str[i].charCodeAt(0) !== previousLetterNumber + 1){
+            missingLetter = String.fromCharCode(previousLetterNumber + 1)
+            break;
+        } else {
+            previousLetterNumber++;
+        }
+    }
+    // returns undefined if no missing characters
+    return missingLetter;
+}
+
+function missingLetters_version02(str: string): string {
+    let previousLetterNumber: number = str[0].charCodeAt(0);
+    for (let i: number = 1; i < str.length; i++) {
+        if(str[i].charCodeAt(0) !== previousLetterNumber + 1){
+            return String.fromCharCode(previousLetterNumber + 1)
+        } else {
+            previousLetterNumber++;
+        }
+    }
+    return; // returns undefined by design
+}
+
+function missingLetters_version01(str: string): string {
     let missingLetter: string;
     let currentLetterNumber: number = str[0].charCodeAt(0);
     for (let i: number = 1; i < str.length; i++) {
