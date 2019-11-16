0bebaeeb02f1a341012a7b2cf08f92d7943dbb6e
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-16 08:46:39 -0800

    add dylan's version to a_074

--- 100AlgorithmsChallenge/074-maxMultiple/maxMultiple.ts
+++ 100AlgorithmsChallenge/074-maxMultiple/maxMultiple.ts
@@ -1,4 +1,10 @@
+// dylan's version.  Just a little cleaner
 function maxMultiple(divisor: number, bound: number): number  {
+    let remainder: number = bound % divisor;
+    return bound - remainder;
+}
+
+function maxMultiple_mine(divisor: number, bound: number): number  {
     let largestMultiple: number = Math.floor(bound / divisor);
     return divisor * largestMultiple;
 }
