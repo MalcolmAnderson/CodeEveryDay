e0cec36a2c20781e23bf599e27966a71e3519ffc
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-16 08:32:31 -0800

    a_077 - kind of a a lotta fun

--- 100AlgorithmsChallenge/077-missingLetters/missingLetters.ts
+++ 100AlgorithmsChallenge/077-missingLetters/missingLetters.ts
@@ -1,6 +1,16 @@
 function missingLetters(str: string): string {
-   
+    let missingLetter: string;
+    let currentLetterNumber: number = str[0].charCodeAt(0);
+    for (let i: number = 1; i < str.length; i++) {
+        if(str[i].charCodeAt(0) !== currentLetterNumber + 1){
+            return String.fromCharCode(currentLetterNumber + 1)
+        } else {
+            currentLetterNumber++;
+        }
+    }
+    return missingLetter;
 }
+
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
 console.log(missingLetters("abcdefghjklmno"));
