ac3260ca1cf28c97a95b1209989f1d9f08a3f1e8
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-16 09:43:47 -0800

    added a few more versions

--- 100AlgorithmsChallenge/077-missingLetters/missingLetters.ts
+++ 100AlgorithmsChallenge/077-missingLetters/missingLetters.ts
@@ -1,4 +1,29 @@
+// dylan's version
 function missingLetters(str: string): string {
+    const alphabet: string[] = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let chars = str.split('');
+    for(let i: number = 0; i < chars.length; i++){
+        if(chars[i] !== alphabet[i]) {
+            return alphabet[i];
+        }
+    }
+    return;
+}
+
+// my variation on dylan's version
+function missingLetters_version04(str: string): string {
+    const alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+    // find first str[] letter in alphabet
+    let letterOffset = alphabet.indexOf(str[0]);
+    for(let i: number = 0; i < str.length; i++){
+        if(alphabet[i + letterOffset] !== (str[i])){
+            return alphabet[i + letterOffset];
+        }
+    }
+    return;
+}
+
+function missingLetters_version03(str: string): string {
     let missingLetter: string;  // undefined by design
     let previousLetterNumber: number = str[0].charCodeAt(0);
     for (let i: number = 1; i < str.length; i++) {
@@ -38,6 +63,7 @@ function missingLetters_version01(str: string): string {
     return missingLetter;
 }
 
+// answer depends on whether "bce" should return 'a' or 'd';
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
 console.log(missingLetters("abcdefghjklmno"));
