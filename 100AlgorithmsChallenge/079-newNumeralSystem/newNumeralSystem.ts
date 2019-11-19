a5c9b6a4c8d894585176d3b8f8be9e01ad4528a7
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-19 11:49:57 -0800

    a_079 was non-trivial ... even challenging.

--- 100AlgorithmsChallenge/079-newNumeralSystem/newNumeralSystem.ts
+++ 100AlgorithmsChallenge/079-newNumeralSystem/newNumeralSystem.ts
@@ -1,5 +1,31 @@
 function newNumeralSystem(number: string): string[] {
-
+    const letters: object = getLetterCodeObject();
+    const mathFacts: string[] = [];
+    //console.log(letters['Z']);
+    let oneDigitNumber: number = letters[number];
+    //console.log(oneDigitNumber);
+    let halfway: number = Math.floor(oneDigitNumber / 2);
+    for(let i: number = 0; i <= halfway; i++){
+        let partA: string = getLetterCodeFromNumber(i);
+        let partB: string = getLetterCodeFromNumber(oneDigitNumber - i);
+        mathFacts.push(partA + ' + ' + partB);
+    }
+    return mathFacts;
 }   
 
+function getLetterCodeFromNumber(num: number): string{
+    let capANumberCode = 'A'.charCodeAt(0);
+    return String.fromCharCode(capANumberCode + num);
+}
+
+function getLetterCodeObject(): object{
+    const letters: object = {}
+    let capANumberCode = 'A'.charCodeAt(0);
+    for(let i: number = 0; i < 26; i++){
+        letters[String.fromCharCode(capANumberCode + i)] = i;
+    }
+    return letters;
+}
+
 console.log(newNumeralSystem('G'));
+console.log(newNumeralSystem('H'));
