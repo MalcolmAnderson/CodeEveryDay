89d310432df38923655761b2215544be1e38dd21
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-20 07:45:26 -0800

    prep unit tests

--- 100AlgorithmsChallenge/080-pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ 100AlgorithmsChallenge/080-pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,3 +1,12 @@
 function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
-
+    
 }
+
+//-   For current = 1 and numberOfDigits = 5, the output should be
+console.log(pagesNumberingWithInk(1, 5)); // 5 - 1, 2, 3, 4, 5.
+
+//-   For current = 21 and numberOfDigits = 5, the output should be
+console.log(pagesNumberingWithInk(21, 5)); // 22 - 21, 22
+
+// -   For current = 8 and numberOfDigits = 4, the output should be
+console.log(pagesNumberingWithInk(8, 4)); // 10 - 8, 9, 10
