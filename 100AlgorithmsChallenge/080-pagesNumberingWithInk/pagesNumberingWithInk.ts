6ca206ff31d1e73d0d4c5dc04399a14e8b6bd529
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-20 08:02:32 -0800

    first pass

--- 100AlgorithmsChallenge/080-pagesNumberingWithInk/pagesNumberingWithInk.ts
+++ 100AlgorithmsChallenge/080-pagesNumberingWithInk/pagesNumberingWithInk.ts
@@ -1,5 +1,19 @@
 function pagesNumberingWithInk(current: number, numberOfDigits: number): number {
-    
+    let lastPage: number = current - 1;
+    let digitsUsed: number = 0;
+    while(digitsUsed < numberOfDigits){
+        if(lastPage + 1 < 10){
+            lastPage++;
+            digitsUsed++;
+        } else if (lastPage + 1 < 100 && numberOfDigits - digitsUsed >= 2) {
+            lastPage++;
+            digitsUsed += 2;
+        } else {
+            console.log(numberOfDigits - digitsUsed);
+            break;
+        }
+    }
+    return lastPage;
 }
 
 //-   For current = 1 and numberOfDigits = 5, the output should be
