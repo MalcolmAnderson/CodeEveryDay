f88f2700edefc9d38b24f32814cd186e1f06ca25
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-22 07:43:01 -0800

    completed a_081 - lots to learn on this one

--- 100AlgorithmsChallenge/081-palindromeRearranging/palindromeRearranging.ts
+++ 100AlgorithmsChallenge/081-palindromeRearranging/palindromeRearranging.ts
@@ -1,5 +1,32 @@
 function palindromeRearranging(inputString: string): boolean {
+    const letters: object = getLetterCodeObject();
+    for(let i: number = 0; i < inputString.length; i++){
+        letters[inputString[i].toUpperCase()]++;
+    }
+    let oddCount = 0;
+    for(let element in letters){
+        if(letters[element] % 2 !== 0){
+            oddCount++;
+        }
+    }
+    return oddCount > 1 ? false : true;
+    return true;
+}
 
+function getLetterCodeObject(): object{
+    const letters: object = {}
+    let capANumberCode = 'A'.charCodeAt(0);
+    for(let i: number = 0; i < 26; i++){
+        letters[String.fromCharCode(capANumberCode + i)] = 0;
+    }
+    return letters;
 }
 
 console.log(palindromeRearranging('aabb'));
+console.log(palindromeRearranging('daabb'));
+console.log(palindromeRearranging('edaabb'));
+
+
+
+
+
