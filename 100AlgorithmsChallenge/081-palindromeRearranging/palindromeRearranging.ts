ff021f5d104cfd8ae3d301576a0c4455661b6a4a
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-22 07:49:58 -0800

    a_081 remove pre-built letter bag

--- 100AlgorithmsChallenge/081-palindromeRearranging/palindromeRearranging.ts
+++ 100AlgorithmsChallenge/081-palindromeRearranging/palindromeRearranging.ts
@@ -1,7 +1,13 @@
 function palindromeRearranging(inputString: string): boolean {
-    const letters: object = getLetterCodeObject();
+    const letters: object = {}
+
     for(let i: number = 0; i < inputString.length; i++){
-        letters[inputString[i].toUpperCase()]++;
+        const thisLetter = inputString[i].toUpperCase();
+        if(letters.hasOwnProperty(thisLetter)){
+            letters[thisLetter]++;
+        } else {
+            letters[thisLetter] = 1;
+        }
     }
     let oddCount = 0;
     for(let element in letters){
@@ -10,17 +16,16 @@ function palindromeRearranging(inputString: string): boolean {
         }
     }
     return oddCount > 1 ? false : true;
-    return true;
 }
 
-function getLetterCodeObject(): object{
-    const letters: object = {}
-    let capANumberCode = 'A'.charCodeAt(0);
-    for(let i: number = 0; i < 26; i++){
-        letters[String.fromCharCode(capANumberCode + i)] = 0;
-    }
-    return letters;
-}
+// function getLetterCodeObject(): object{
+//     const letters: object = {}
+//     let capANumberCode = 'A'.charCodeAt(0);
+//     for(let i: number = 0; i < 26; i++){
+//         letters[String.fromCharCode(capANumberCode + i)] = 0;
+//     }
+//     return letters;
+// }
 
 console.log(palindromeRearranging('aabb'));
 console.log(palindromeRearranging('daabb'));
