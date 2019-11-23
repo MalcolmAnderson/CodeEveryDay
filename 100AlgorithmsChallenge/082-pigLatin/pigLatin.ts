06480b5defc1f5bea007d89dde9981c374edb350
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-23 15:37:46 -0800

    a_082 - it works, but it's not elegant ... at ALL

--- 100AlgorithmsChallenge/082-pigLatin/pigLatin.ts
+++ 100AlgorithmsChallenge/082-pigLatin/pigLatin.ts
@@ -1,6 +1,31 @@
 function pigLatin(str: string): string {
-
+    const igpayAtinLay: string[] = [];
+    let locationOfFirstVowel = getFirstVowel(str);
+    if(locationOfFirstVowel === 0){
+        return str.concat("way");
+    } else {
+        for(let i: number = locationOfFirstVowel; i < str.length; i++){
+            igpayAtinLay.push(str[i]);
+        }
+        for(let i: number = 0; i < locationOfFirstVowel; i++){
+            igpayAtinLay.push(str[i]);
+        }
+        igpayAtinLay.push('ay');
+    }
+    return igpayAtinLay.join('');
 }
 
-console.log(pigLatin("glove"));
-console.log(pigLatin("eight"));
+
+function getFirstVowel(igpayAtinLay: string): number{
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    for(let i: number = 0; i < igpayAtinLay.length; i++){
+        if(vowels.has(igpayAtinLay[i])){
+            return i;
+        }
+    }
+}
+
+
+console.log(pigLatin("glove")); // "oveglay"
+console.log(pigLatin("eight")); // "eightway"
+console.log(pigLatin("stream")); // "eamstray"
