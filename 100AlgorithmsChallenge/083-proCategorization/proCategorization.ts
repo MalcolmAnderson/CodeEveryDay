2f26ef2bd7e31e561c85a5509a973b472bafaf9b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-25 14:26:50 -0800

    a_083 solution works

--- 100AlgorithmsChallenge/083-proCategorization/proCategorization.ts
+++ 100AlgorithmsChallenge/083-proCategorization/proCategorization.ts
@@ -1,54 +1,55 @@
 function proCategorization(pros: string[], preferences: string[][]): string[][][] {
-    const skills: object = {};
-    const outputArray: string[][][] = [];
-    for (let pro: number = 0; pro < pros.length; pro++) {
-        for (let job: number = 0; job < preferences[pro].length; job++) {
-            if (skills.hasOwnProperty(preferences[pro][job])) {
-                skills[preferences[pro][job]].push(pros[pro]);
-            } else {
-                skills[preferences[pro][job]] = pros;
-            }
-        }
-    }
+    const skills: object = populateSkillsObject(pros, preferences);
     console.log(skills);
 
     const skillList: string[] = [];
     for (let subSkill in skills) {
         skillList.push(subSkill);
     }
-    console.log(skills);
-    //console.log(skills["Handyman"]);
-    //console.log(skills["Handyman"].length);
     console.log(skillList);
-    console.log(skillList.length)
-    for(let skill: number = 0; skill < skillList.length; skill++){
+    skillList.sort();
+
+    const outputArray: string[][][] = [];
+    
+
+    for (let skillAsNumber: number = 0; skillAsNumber < skillList.length; skillAsNumber++) {
         const outputRow: string[][] = [];
         const thisSkill: string[] = [];
-        thisSkill.push(skillList[skill]);
-        console.log(skillList[skill]);
+        thisSkill.push(skillList[skillAsNumber]);
         outputRow.push(thisSkill);
-        
-        console.log(skills[skillList[skill]]);
-        console.log(skills[skillList[skill]].length);
-        const professional: string[] = [];
-        for(let j: number = 0; j < skills[skillList[skill]].length; j++){
-            console.log(skills[skillList[skill]][j]);
-             professional.push(skills[skillList[skill]][j]);
-             console.log(professional);
-        }
-        outputRow.push(professional);
+        const professionalsWithThisSkill = skills[skillList[skillAsNumber]]
+        outputRow.push(professionalsWithThisSkill);
         outputArray.push(outputRow);
     }
-    console.log(outputArray.length)
-    console.log(outputArray[0].length)
     return outputArray;
+}
+
+
+
+function populateSkillsObject(pros: string[], preferences: string[][]): object {
+    const skills: object = {};
+    for (let pro: number = 0; pro < pros.length; pro++) {
+        console.log(preferences[pro].length)
+        for (let job: number = 0; job < preferences[pro].length; job++) {
+            console.log(skills);
+            console.log(preferences[pro][job]);
+            if (skills.hasOwnProperty(preferences[pro][job])) {
+                skills[preferences[pro][job]].push(pros[pro]);
+            } else {
+                skills[preferences[pro][job]] = [pros[pro]];
+                console.log(skills[preferences[pro][job]])
+            }
+        }
+    }
+    console.log(skills);
+    return skills;
 }
 
 
 // [ [ [ "Computer repair" ], [ "Jack" ] ] ];
 //console.log(proCategorization(["Jack"], [["Computer repair"]]));
 
-console.log(proCategorization(["Jack", "Leon"], [ ["Computer repair"], ["Computer lessons"] ] ));
+//console.log(proCategorization(["Jack", "Leon"], [["Computer repair"], ["Computer lessons"]]));
 
 // [ 
 //    [ 
@@ -77,6 +78,10 @@ console.log(proCategorization(["Jack", "Leon"], [ ["Computer repair"], ["Compute
 //                                         [["House cleaning"], ["Jack", "Maria"]]]
 
 
-// console.log(proCategorization(["Jack", "Leon", "Maria"], [["Computer repair", "Handyman", "House cleaning"],
-// ["Computer lessons", "Computer repair", "Data recovery service"],
-// ["Computer lessons", "House cleaning"]]));
+
+
+
+
+console.log(proCategorization(["Jack", "Leon", "Maria"], [["Computer repair", "Handyman", "House cleaning"],
+["Computer lessons", "Computer repair", "Data recovery service"],
+["Computer lessons", "House cleaning"]]));
