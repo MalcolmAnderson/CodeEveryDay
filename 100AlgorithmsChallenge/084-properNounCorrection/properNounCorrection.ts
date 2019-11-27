4911dead401c5a086270f5b42cd3ff61cce5b38f
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-27 11:57:26 -0800

    a_084 - simple one and done

--- 100AlgorithmsChallenge/084-properNounCorrection/properNounCorrection.ts
+++ 100AlgorithmsChallenge/084-properNounCorrection/properNounCorrection.ts
@@ -1,5 +1,10 @@
 function properNounCorrection(noun: string): string {
-   
+   const output: string[] = [];
+   output.push(noun[0].toUpperCase());
+   for(let i: number = 1; i < noun.length; i++){
+       output.push(noun[i].toLocaleLowerCase());
+   }
+   return output.join('');
 }
 
 console.log(properNounCorrection('pARiS'));
