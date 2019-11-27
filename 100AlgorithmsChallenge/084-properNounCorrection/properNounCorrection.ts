03d85e80ec76df575ea10e79415a4a1504d3c7c9
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-27 13:09:21 -0800

    a_084 added Dylan's version

--- 100AlgorithmsChallenge/084-properNounCorrection/properNounCorrection.ts
+++ 100AlgorithmsChallenge/084-properNounCorrection/properNounCorrection.ts
@@ -1,10 +1,17 @@
+// Dylan's version
 function properNounCorrection(noun: string): string {
-   const output: string[] = [];
-   output.push(noun[0].toUpperCase());
-   for(let i: number = 1; i < noun.length; i++){
-       output.push(noun[i].toLocaleLowerCase());
-   }
-   return output.join('');
+    const firstChar = noun[0].toUpperCase();
+    const restOfWord = noun.slice(1, noun.length).toLowerCase();
+    return firstChar.concat(restOfWord);
+}
+
+function properNounCorrection_mine(noun: string): string {
+    const output: string[] = [];
+    output.push(noun[0].toUpperCase());
+    for (let i: number = 1; i < noun.length; i++) {
+        output.push(noun[i].toLocaleLowerCase());
+    }
+    return output.join('');
 }
 
 console.log(properNounCorrection('pARiS'));
