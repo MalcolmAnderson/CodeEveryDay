3e6894be1275cfa0e3908c866ec94c77d31e7590
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-11-29 13:49:47 -0800

    a_085 version 1

--- 100AlgorithmsChallenge/085-ratingThreshold/ratingThreshold.ts
+++ 100AlgorithmsChallenge/085-ratingThreshold/ratingThreshold.ts
@@ -1,5 +1,15 @@
 function ratingThreshold(threshold: number, ratings: number[][]): number[] {
-
+    const belowThreshold: number[] = [];
+    for(let i: number = 0; i < ratings.length; i++){
+        let sum: number = 0;
+        for(let j: number = 0; j < ratings[i].length; j++){
+            sum += ratings[i][j];
+        }
+        if(sum / ratings[i].length < threshold){
+            belowThreshold.push(i);
+        }
+    }
+    return belowThreshold;
 }
 
 console.log(ratingThreshold(3.5, 
