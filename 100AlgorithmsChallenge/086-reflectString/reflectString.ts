357bc407b7bbc08f66011b77462ee01de201bec3
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-01 12:57:44 -0800

    added Dylan and Caleb's version

--- 100AlgorithmsChallenge/086-reflectString/reflectString.ts
+++ 100AlgorithmsChallenge/086-reflectString/reflectString.ts
@@ -1,4 +1,41 @@
-function reflectString(inputString: string): string {
+// Caleb E's version (crazy slick)
+function reflectString(inputString: string): any {
+    const firstLetterCode = 'a'.charCodeAt(0);
+    const alphabetRange = 25; // zero based index
+
+    return inputString
+        .toLowerCase()
+        .split('')
+        .map(char => {
+            // must be alphabetic character
+            if (char.match(/\W|\d/)) return char;
+
+            const alphaBased0 = char.charCodeAt(0) - firstLetterCode;
+            const mirrorAlpha = alphabetRange - alphaBased0;
+            return String.fromCharCode(firstLetterCode + mirrorAlpha);
+        })
+        .join('');
+}
+
+function reflectString_dylans(inputString: string): string {
+    const chars: string[] = inputString.split('');
+    const reflectionLetters: object = {
+        a: 'z', b: 'y', c: 'x', d: 'w',
+        e: 'v', f: 'u', g: 't', h: 's',
+        i: 'r', j: 'q', k: 'p', l: 'o',
+        m: 'n', n: 'm', o: 'l', p: 'k',
+        q: 'j', r: 'i', s: 'h', t: 'g',
+        u: 'f', v: 'e', w: 'd', x: 'c',
+        y: 'b', z: 'a'
+    }
+    let reflectedWord = '';
+    chars.forEach((char) => {
+        reflectedWord += reflectionLetters[char];
+    })
+    return reflectedWord;
+}
+
+function reflectString_mine(inputString: string): string {
     const output: string[] = [];
     inputString = inputString.toLowerCase();
     for (let i: number = 0; i < inputString.length; i++) {
