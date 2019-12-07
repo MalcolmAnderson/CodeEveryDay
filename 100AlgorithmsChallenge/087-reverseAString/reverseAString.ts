2598300b7064527bf7d76f45b267d3b05c9fb075
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-07 12:33:31 -0800

    update comments

--- 100AlgorithmsChallenge/087-reverseAString/reverseAString.ts
+++ 100AlgorithmsChallenge/087-reverseAString/reverseAString.ts
@@ -1,8 +1,9 @@
-function reverseAString(str: string): string {
+function reverseAString_DylansFirstVersion(str: string): string {
     return str.split('').reverse().join('');
 }
 
-function reverseAString_mine(str: string): string {
+// Mine was same as Dylan's second verison
+function reverseAString(str: string): string {
     let output: string = "";
     for (let i: number = str.length - 1; i >= 0; i--) {
         output += str[i];
