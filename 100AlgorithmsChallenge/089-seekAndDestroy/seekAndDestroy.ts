957408319ceea09994e0bb229ef3cd839238b7bd
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-07 12:48:43 -0800

    added Dylan's and Nazareth's 2 solutions

--- 100AlgorithmsChallenge/089-seekAndDestroy/seekAndDestroy.ts
+++ 100AlgorithmsChallenge/089-seekAndDestroy/seekAndDestroy.ts
@@ -1,4 +1,28 @@
-function seekAndDestroy(arr1: number[], arr2: number[]): number[] {
+// Nazareth's solution to Dylan's object solution
+// TODO create an initialArray with 10,000 elements and do time trials for each method
+function seekAndDestroy(initialArray: number[], itemsToBeRemoved: number[]): number[] {
+
+    let results: number[] = [], obj = {};
+
+    for (let i = 0; i < itemsToBeRemoved.length; i++) {
+        obj[itemsToBeRemoved[i]] = i;
+    }
+
+    for (let i = 0; i < initialArray.length; i++) {
+        if (!obj.hasOwnProperty(initialArray[i])) {
+            results.push(initialArray[i]);
+        }
+    }
+
+    return results;
+}
+
+// Dylan's filter solution
+function seekAndDestroy_DylansFilterSolution(arr1: number[], arr2: number[]): number[] {
+    return arr1.filter(element => !arr2.includes(element));
+}
+
+function seekAndDestroy_myFilterSolution(arr1: number[], arr2: number[]): number[] {
     return arr1.filter(num => arr2.indexOf(num) === -1);
 }
 
