4299b43e3ea04e6e6a92771ac6f4410edbd9f3bc
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-09 09:18:07 -0800

    a_100 was a lot of fun - I'm interested in Dylan's

--- 100AlgorithmsChallenge/100-sortByHeight/sortByHeight.ts
+++ 100AlgorithmsChallenge/100-sortByHeight/sortByHeight.ts
@@ -1,5 +1,22 @@
 function sortByHeight(a: number[]): number[] {
-
+    const output: number[] = [];
+    const locationOfTrees: number[] = [];
+    const heightOfPeople: number[] = [];
+    for (let i: number = 0; i < a.length; i++) {
+        a[i] === -1 ? locationOfTrees.push(i) : heightOfPeople.push(a[i]);
+    }
+    heightOfPeople.sort((a, b) => a - b);
+    let treesIndex: number = 0;
+    let peopleIndex: number = 0;
+    for (let i: number = 0; i < a.length; i++) {
+        if (locationOfTrees[treesIndex] === i) {
+            treesIndex++;
+            output.push(-1);
+        } else {
+            output.push(heightOfPeople[peopleIndex++]);
+        }
+    }
+    return output;
 }
-
+//                                                                 [ -1, 150, 160, 170, -1, -1, 180, 190 ]
 console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
