335873744f5af58c657202cefe63801242fab5fa
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-10 08:36:33 -0800

    a_100 add dylan's version

--- 100AlgorithmsChallenge/100-sortByHeight/sortByHeight.ts
+++ 100AlgorithmsChallenge/100-sortByHeight/sortByHeight.ts
@@ -1,4 +1,23 @@
+// Dylan's
 function sortByHeight(a: number[]): number[] {
+    const array2 = a.filter((element) => {
+        if (element != -1) {
+            return element;
+        }
+    }).sort((num1, num2) => num1 - num2);
+
+    let indexVal = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] != -1) {
+            a[i] = array2[indexVal]; // indexVal++ here would have saved a line
+            indexVal++;
+        }
+    }
+    return a;
+}
+
+function sortByHeight_mine(a: number[]): number[] {
     const output: number[] = [];
     const locationOfTrees: number[] = [];
     const heightOfPeople: number[] = [];
