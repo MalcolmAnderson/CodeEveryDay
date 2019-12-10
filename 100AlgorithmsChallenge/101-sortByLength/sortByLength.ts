6ca8f2d8e2439fe92541d4ef51e194e0d586ca5b
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-10 08:27:41 -0800

    a_101 - interesting challenge

--- 100AlgorithmsChallenge/101-sortByLength/sortByLength.ts
+++ 100AlgorithmsChallenge/101-sortByLength/sortByLength.ts
@@ -1,9 +1,30 @@
 function sortByLength(inputArray: string[]): string[] {
+    const arrayObject: object = {};
+    let largestArraySize: number = 0;
+    for (let i: number = 0; i < inputArray.length; i++) {
+        let currentSize = inputArray[i].length;
+        if (currentSize > largestArraySize) {
+            largestArraySize = currentSize;
+        }
+        if (!arrayObject.hasOwnProperty(currentSize)) {
+            arrayObject[currentSize] = [];
+        }
+        arrayObject[currentSize].push(inputArray[i]);
+    }
+    let outputArray: string[] = [];
+    for (let i: number = 0; i <= largestArraySize; i++) {
+        if (arrayObject.hasOwnProperty(i)) {
+            for (let j: number = 0; j < arrayObject[i].length; j++) {
+                outputArray.push(arrayObject[i][j]);
+            }
+        }
+    }
+    return outputArray;
 
 }
 
 console.log(sortByLength(["abc",
-"",
-"aaa",
-"a",
-"zz"]));
+    "",
+    "aaa",
+    "a",
+    "zz"]));
