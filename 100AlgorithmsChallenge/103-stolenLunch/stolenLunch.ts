24200a4e8f30547ad5e1d52ea97b50afa5b0a316
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-13 09:25:53 -0800

    next, get string after : and covert back from char

--- 100AlgorithmsChallenge/103-stolenLunch/stolenLunch.ts
+++ 100AlgorithmsChallenge/103-stolenLunch/stolenLunch.ts
@@ -1,4 +1,8 @@
-function stolenLunch(note: string): string {
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+function stolenLunch_firstPass_ReadTheRequirements(note: string): string {
     const char2num: object = {
         "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7,
         "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13,
@@ -9,9 +13,31 @@ function stolenLunch(note: string): string {
     const restOfString = strAsArray.splice(strAsArray.indexOf(' '));
     console.log(strAsArray.join(''))
     console.log(restOfString.join(''));
-
+    console.log(char2num.hasOwnProperty("a"));
+    console.log(getKeyByValue(char2num, ";"))
+    for (let i: number = 0; i < restOfString.length; i++) {
+        if (char2num.hasOwnProperty(restOfString[i])) {
+            strAsArray.push(char2num[char2num[i]]);
+        }
+    }
 
     return strAsArray.join('');
 }
+console.log(isNaN(' '));
+function stolenLunch(note: string): string {
+    const strAsArray: string[] = note.split('');
+    const outPutArray: string[] = [];
+    for (let i: number = 0; i < strAsArray.length; i++) {
+        if (isNaN(Number(strAsArray[i])) || strAsArray[i] === ' ') {
+            outPutArray.push(strAsArray[i]);
+        } else {
+            outPutArray.push(String.fromCharCode('a'.charCodeAt(0) + parseInt(strAsArray[i])));
+        }
+    }
+    return outPutArray.join('');
+}
+
+console.log('c'.charCodeAt(0) - 'a'.charCodeAt(0));
+
 //                                                       "you'll never guess it: 2390"
 console.log(stolenLunch("you'll n4v4r 6u4ss 8t: cdja")); 
