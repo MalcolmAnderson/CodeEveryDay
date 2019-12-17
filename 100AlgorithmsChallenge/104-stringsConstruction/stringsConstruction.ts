59c98baa47e37b495672c5bead048e71b4de3bd1
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-17 08:05:55 -0800

    a_104 - complex but fun

--- 100AlgorithmsChallenge/104-stringsConstruction/stringsConstruction.ts
+++ 100AlgorithmsChallenge/104-stringsConstruction/stringsConstruction.ts
@@ -1,5 +1,29 @@
 function stringsConstruction(a: string, b: string): number {
-
+    const bag: object = {};
+    for (let i: number = 0; i < b.length; i++) {
+        if (!bag.hasOwnProperty(b[i])) {
+            bag[b[i]] = 1;
+        } else {
+            bag[b[i]]++;
+        }
+    }
+    let continueLoop: Boolean = true;
+    let count: number = 0;
+    while (continueLoop) {
+        for (let i: number = 0; i < a.length; i++) {
+            if (bag.hasOwnProperty(a[i]) && bag[a[i]] > 0) {
+                bag[a[i]]--;
+            } else {
+                continueLoop = false;
+            }
+        }
+        if (continueLoop) {
+            count++;
+        }
+    }
+    return count;
 }
 
 console.log(stringsConstruction('abc', 'abccba'));
+console.log(stringsConstruction('ab', 'abbaab'));
+console.log(stringsConstruction('a', 'aaaaaaa'));
