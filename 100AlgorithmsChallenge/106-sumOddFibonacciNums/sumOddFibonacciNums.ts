a4d8cbe3df0afec88a1adc453e375faa6a91aaa5
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-19 08:07:40 -0800

    add tests

--- 100AlgorithmsChallenge/106-sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ 100AlgorithmsChallenge/106-sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -2,6 +2,10 @@ function sumOddFibonacciNums(num: number): number {
 
 }
 
-console.log(sumOddFibonacciNums(1000));
-console.log(sumOddFibonacciNums(4000000));
+
+//                                    10
 console.log(sumOddFibonacciNums(10));
+//                                      1785
+console.log(sumOddFibonacciNums(1000));
+//                                         4613732
+console.log(sumOddFibonacciNums(4000000));
