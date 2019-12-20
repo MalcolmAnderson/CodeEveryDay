346f210a7c83684c329be39dfb97610f52484d32
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-20 07:55:01 -0800

    make sure everything is saved

--- 100AlgorithmsChallenge/106-sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ 100AlgorithmsChallenge/106-sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,11 +1,28 @@
 function sumOddFibonacciNums(num: number): number {
-    return 10;
+    if (num < 1) { return 0; }
+    if (num === 1) { return 1; }
+    const FibSequence: number[] = [1, 1];
+    // build FibSequence
+    let last: number = 1
+    let current: number = 2
+    while (current <= num) {
+        FibSequence.push(current);
+        const total: number = last + current;
+        last = current;
+        current = total;
+    }
+    // sum the odd numbers
+    let total: number = 0;
+    FibSequence.forEach(element => {
+        total += element % 2 === 0 ? 0 : element;
+    });
+    return total;
 }
 
 
-//                                    10
+//                                     10
 console.log(sumOddFibonacciNums(10));
-//                                      1785
+//                                       1785
 console.log(sumOddFibonacciNums(1000));
-//                                         4613732
+//                                          4613732
 console.log(sumOddFibonacciNums(4000000));
