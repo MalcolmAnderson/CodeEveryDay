7e3e0e0e75695fa5c430c6072306d50a060412c5
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-24 08:04:45 -0800

    trickier than expected

--- 100AlgorithmsChallenge/108-switchLights/switchLights.ts
+++ 100AlgorithmsChallenge/108-switchLights/switchLights.ts
@@ -1,7 +1,30 @@
 function switchLights(a: number[]): number[] {
-
+    const stringArray: string[] = a.toString().split(',');
+    for (let i: number = 0; i < a.length; i++) {
+        if (a[i] === 1) {
+            flipAllBitsUpToX(stringArray, i);
+        }
+    }
+    const outputArray: number[] = [];
+    stringArray.forEach(element => {
+        outputArray.push(parseInt(element));
+    });
+    return outputArray;
+}
+function flipAllBitsUpToX(sA: string[], x: number): string[] {
+    for (let i: number = 0; i <= x; i++) {
+        if (sA[i] === "1") {
+            sA[i] = "0";
+        } else {
+            sA[i] = "1";
+        }
+    }
+    return sA;
 }
 
-
-console.log(switchLights([1, 1, 1, 1, 1]));
+//                                  [ 0, 0 ]
 console.log(switchLights([0, 0]));
+//                                           [ 0, 1, 0, 1, 0 ]
+console.log(switchLights([1, 1, 1, 1, 1]));
+//                                           [ 0, 1, 1, 0, 0 ]
+console.log(switchLights([0, 1, 0, 1, 0]));
