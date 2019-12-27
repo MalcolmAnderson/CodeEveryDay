d0c37e414e0206e430957339e3b1e84dbbb573ba
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2019-12-27 07:58:54 -0800

    version 1 of a_110 - v2 soon

--- 100AlgorithmsChallenge/110-uniqueDigitProducts/uniqueDigitsProducts.ts
+++ 100AlgorithmsChallenge/110-uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,28 @@
 function uniqueDigitProducts(a: number[]): number {
+    const outArray: number[] = [];
+    for (let i: number = 0; i < a.length; i++) {
+        outArray.push(returnProductNumber(a[i]));
+    }
+    const uniquePNs: number[] = [];
+    for (let i: number = 0; i < outArray.length; i++) {
+        console.log(outArray[i]);
+        if (uniquePNs.indexOf(outArray[i]) === -1) {
+            uniquePNs.push(outArray[i]);
+        }
+    }
+    // function to return product number
+    // capture those numbers
+    // count the unique numbers
+    return uniquePNs.length;
+}
+
+function returnProductNumber(a: number): number {
+    const strArray: string[] = a.toString().split('');
+    let total = 1;
+    for (let i: number = 0; i < strArray.length; i++) {
+        total *= parseInt(strArray[i]);
+    }
+    return total;
 }
 
 console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
