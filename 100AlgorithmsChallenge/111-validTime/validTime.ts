a73b227b6e4d5a383d8f6966c153594a7c841262
Malcolm Anderson <malcolm.b.anderson@gmail.com>
2020-01-03 07:13:19 -0800

    fun

--- 100AlgorithmsChallenge/111-validTime/validTime.ts
+++ 100AlgorithmsChallenge/111-validTime/validTime.ts
@@ -1,5 +1,8 @@
 function validTime(time: string): boolean {
-
+    const hours: number = parseInt(time.substr(0, 2));
+    const minutes: number = parseInt(time.substr(time.length - 2, 2));
+    //console.log(time.substr(time.length - 3, 2))
+    return hours < 25 && minutes < 61;
 }
 
 console.log(validTime('13:58'));
